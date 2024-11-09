9d126875245b586f49ff0a0a080f99b437d66f6e
r-sp <36327242+r-sp@users.noreply.github.com>
2024-11-09 13:30:20 +0700

    add header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
 import { type ReactNode } from "react";
-import { CounterStoreProvider } from "./provider";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import clsx from "clsx";
+import Header from "~/components/header";
+import Footer from "~/components/footer";
+import "./style.css";
 
 export default function RootLayout(props: Readonly<{ children: ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={clsx(GeistSans.variable, GeistMono.variable)}>
       <body>
-        <CounterStoreProvider>{props.children}</CounterStoreProvider>
+        <Header />
+        <main>{props.children}</main>
+        <Footer />
       </body>
     </html>
   );
