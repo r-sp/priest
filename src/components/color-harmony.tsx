a88b806d1c4c58102e3595c8c8c64205b2f1f493
r-sp <36327242+r-sp@users.noreply.github.com>
2024-11-12 23:28:28 +0700

    combine color harmony tab

--- src/components/color-harmony.tsx
+++ src/components/color-harmony.tsx
@@ -1,35 +1,100 @@
 "use client";
 
-import { colord, extend } from "colord";
-import ColorTab from "./color-tab";
+import { useState } from "react";
+import clsx from "clsx";
 
-import harmoniesPlugin from "colord/plugins/harmonies";
-extend([harmoniesPlugin]);
+type ColorHarmony =
+  | "analogous"
+  | "complementary"
+  | "double-split-complementary"
+  | "rectangle"
+  | "split-complementary"
+  | "tetradic"
+  | "triadic";
 
-export default function ColorHarmony(props: { hsv: { h: number; s: number; v: number; a: number } }) {
-  const color = colord(props.hsv);
-  const colorHEX = color.toHex();
-  const analogous = color.harmonies("analogous").map((c) => c.toHex());
-  const complementary = color.harmonies("complementary").map((c) => c.toHex());
-  const doubleSplitComplementary = color.harmonies("double-split-complementary").map((c) => c.toHex());
-  const rectangle = color.harmonies("rectangle").map((c) => c.toHex());
-  const splitComplementary = color.harmonies("split-complementary").map((c) => c.toHex());
-  const tetradic = color.harmonies("tetradic").map((c) => c.toHex());
-  const triadic = color.harmonies("triadic").map((c) => c.toHex());
+type Colors = {
+  analogous: string[];
+  complementary: string[];
+  doubleSplitComplementary: string[];
+  rectangle: string[];
+  splitComplementary: string[];
+  tetradic: string[];
+  triadic: string[];
+};
 
-  const colors = {
-    analogous: analogous,
-    complementary: complementary,
-    doubleSplitComplementary: doubleSplitComplementary,
-    rectangle: rectangle,
-    splitComplementary: splitComplementary,
-    tetradic: tetradic,
-    triadic: triadic,
+export default function ColorHarmony(props: { harmony: Colors }) {
+  const [content, setContent] = useState<ColorHarmony>("double-split-complementary");
+
+  const ColorCard = (props: { color: string[] }) => {
+    return (
+      <div className="color-harmony">
+        {props.color.map((color, index) => (
+          <div className="color-card" key={index}>
+            <div className="color-display" style={{ backgroundColor: color }}></div>
+            <code className="color-name" role="none">
+              {color}
+            </code>
+          </div>
+        ))}
+      </div>
+    );
   };
 
   return (
     <div className="color-harmonies">
-      <ColorTab harmony={colors} hex={colorHEX} />
+      <nav className="color-tab" aria-label="color harmony">
+        <button
+          className={clsx("color-tab-option btn", content === "complementary" && "active")}
+          onClick={() => setContent("complementary")}
+        >
+          <span>Complementary</span>
+        </button>
+        <button
+          className={clsx("color-tab-option btn", content === "analogous" && "active")}
+          onClick={() => setContent("analogous")}
+        >
+          <span>Analogous</span>
+        </button>
+        <button
+          className={clsx("color-tab-option btn", content === "triadic" && "active")}
+          onClick={() => setContent("triadic")}
+        >
+          <span>Triadic</span>
+        </button>
+        <button
+          className={clsx("color-tab-option btn", content === "split-complementary" && "active")}
+          onClick={() => setContent("split-complementary")}
+        >
+          <span>Split Complementary</span>
+        </button>
+        <button
+          className={clsx("color-tab-option btn", content === "tetradic" && "active")}
+          onClick={() => setContent("tetradic")}
+        >
+          <span>Tetradic</span>
+        </button>
+        <button
+          className={clsx("color-tab-option btn", content === "rectangle" && "active")}
+          onClick={() => setContent("rectangle")}
+        >
+          <span>Rectangle</span>
+        </button>
+        <button
+          className={clsx("color-tab-option btn", content === "double-split-complementary" && "active")}
+          onClick={() => setContent("double-split-complementary")}
+        >
+          <span>Double Split Complementary</span>
+        </button>
+      </nav>
+      <section className="color-tab-content" aria-label={clsx("color harmony of ", content.replaceAll("-", " "))}>
+        {content === "complementary" && <ColorCard color={props.harmony.complementary} />}
+        {content === "analogous" && <ColorCard color={props.harmony.analogous} />}
+        {content === "triadic" && <ColorCard color={props.harmony.triadic} />}
+        {content === "split-complementary" && <ColorCard color={props.harmony.splitComplementary} />}
+        {content === "tetradic" && <ColorCard color={props.harmony.tetradic} />}
+        {content === "rectangle" && <ColorCard color={props.harmony.rectangle} />}
+        {content === "double-split-complementary" && <ColorCard color={props.harmony.doubleSplitComplementary} />}
+      </section>
     </div>
   );
 }
