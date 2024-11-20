bd56ccfa7a8a406e8871e5d8c8452d69a313a7f2
r-sp <36327242+r-sp@users.noreply.github.com>
2024-11-20 16:38:09 +0700

    add color store and provider

---
+++ src/components/color/provider.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { type ReactNode, createContext, useRef, useContext } from "react";
+import { type ColorState, createColorStore, initColorStore } from "./store";
+import { convertColor } from "~/lib/color";
+import { useStore } from "zustand";
+
+export type ColorApi = ReturnType<typeof createColorStore>;
+
+export const ColorContext = createContext<ColorApi | undefined>(undefined);
+
+export function ColorProvider({ children }: { children: ReactNode }) {
+  const storeRef = useRef<ColorApi>();
+
+  if (!storeRef.current) {
+    storeRef.current = createColorStore(initColorStore());
+  }
+
+  return <ColorContext.Provider value={storeRef.current}>{children}</ColorContext.Provider>;
+}
+
+export function useColorStore<T>(selector: (store: ColorState) => T) {
+  const colorContext = useContext(ColorContext);
+
+  if (!colorContext) {
+    throw new Error("useColorStore must be used within ColorProvider");
+  }
+
+  return useStore(colorContext, selector);
+}
+
+export function useColorProvider() {
+  const store = useColorStore((state) => state);
+
+  const updateColor = (newColor: typeof store.raw) => {
+    const color = convertColor(newColor);
+
+    return store.update({
+      raw: newColor,
+      hex: color.toHex(),
+      hsl: color.toHsl(),
+      hsv: color.toHsv(),
+      rgb: color.toRgb(),
+    });
+  };
+
+  return {
+    raw: store.raw,
+    hex: store.hex,
+    hsl: store.hsl,
+    hsv: store.hsv,
+    rgb: store.rgb,
+    setRaw: (newColor: typeof store.raw) => updateColor(newColor),
+    setHex: (newColor: typeof store.hex) => updateColor(newColor),
+    setHsl: (newColor: typeof store.hsl) => updateColor(newColor),
+    setHsv: (newColor: typeof store.hsv) => updateColor(newColor),
+    setRgb: (newColor: typeof store.rgb) => updateColor(newColor),
+  };
+}
