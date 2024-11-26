4ac0ddc57f28211661b8e4b1f507a372ed0f8924
r-sp <36327242+r-sp@users.noreply.github.com>
2024-11-26 20:51:53 +0700

    remove extended fonts

--- src/components/font/index.ts
+++ src/components/font/index.ts
@@ -1,14 +1,18 @@
 import localFont from "next/font/local";
 
 export const Geist = localFont({
-  src: [
-    { path: "./geist-sans.woff2", style: "normal" },
-    { path: "./geist-sans-ext.woff2", style: "normal" },
-  ],
+  src: [{ path: "./geist-sans.woff2", style: "normal" }],
   display: "swap",
   weight: "100 900",
   variable: "--geist-sans",
-  declarations: [{ prop: "font-optical-sizing", value: "auto" }],
+  declarations: [
+    { prop: "font-optical-sizing", value: "auto" },
+    {
+      prop: "unicode-range",
+      value:
+        "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD",
+    },
+  ],
   fallback: [
     "ui-sans-serif",
     "system-ui",
@@ -22,14 +26,18 @@ export const Geist = localFont({
 });
 
 export const GeistMono = localFont({
-  src: [
-    { path: "./geist-mono.woff2", style: "normal" },
-    { path: "./geist-mono-ext.woff2", style: "normal" },
-  ],
+  src: [{ path: "./geist-mono.woff2", style: "normal" }],
   display: "swap",
   weight: "100 900",
   variable: "--geist-mono",
-  declarations: [{ prop: "font-optical-sizing", value: "auto" }],
+  declarations: [
+    { prop: "font-optical-sizing", value: "auto" },
+    {
+      prop: "unicode-range",
+      value:
+        "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD",
+    },
+  ],
   fallback: [
     "ui-monospace",
     "SFMono-Regular",
