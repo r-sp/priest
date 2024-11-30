2fb7847928fe733d77b2bf92b47ac268f6f021b5
r-sp <36327242+r-sp@users.noreply.github.com>
2024-11-30 10:55:44 +0700

    fix init value with provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import { type ReactNode } from "react";
 import { Geist, GeistMono } from "~/components/font";
 import { ColorProvider, ColorSlider } from "~/components/color";
-import { getRandomColor } from "~/lib/utils";
+import { initColorProvider } from "~/lib/utils";
 import clsx from "clsx";
 import Link from "next/link";
 import "./style.css";
 
 export default function RootLayout(props: Readonly<{ children: ReactNode }>) {
-  const color = getRandomColor();
+  const color = initColorProvider();
 
   return (
     <html lang="en" className={clsx(Geist.variable, GeistMono.variable)}>
