1516f837708e22b556851fae6b5ee6c8de2300c6
r-sp <36327242+r-sp@users.noreply.github.com>
2024-11-30 21:07:32 +0700

    fix hydration error on client components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import { type ReactNode } from "react";
 import { Geist, GeistMono } from "~/components/font";
-import { ColorProvider, ColorSlider } from "~/components/color";
+import { Header, Footer } from "~/components/ui";
+import { ColorProvider } from "~/components/color";
 import { initColorProvider } from "~/lib/utils";
 import clsx from "clsx";
-import Link from "next/link";
 import "./style.css";
 
 export default function RootLayout(props: Readonly<{ children: ReactNode }>) {
@@ -13,23 +13,10 @@ export default function RootLayout(props: Readonly<{ children: ReactNode }>) {
     <html lang="en" className={clsx(Geist.variable, GeistMono.variable)}>
       <body>
         <ColorProvider initValue={color}>
-          <header>
-            <div className="px-2 xl:mx-auto xl:max-w-screen-xl" role="none">
-              <Link href="/" className="inline-flex py-4 text-base font-medium text-holy-300 max-sm:pointer-events-auto">
-                <span>Priest</span>
-              </Link>
-              <ColorSlider />
-            </div>
-          </header>
+          <Header />
           <main>{props.children}</main>
+          <Footer />
         </ColorProvider>
-        <footer>
-          <div className="px-2 py-12 xl:mx-auto xl:max-w-screen-xl" role="none">
-            <Link href="/color" className="text-sm font-medium text-holy-400">
-              <span>The Holy Colors</span>
-            </Link>
-          </div>
-        </footer>
       </body>
     </html>
   );
