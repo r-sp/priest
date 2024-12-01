0ceb84f980431fdb4ea4154eebea876af11bdf7e
r-sp <36327242+r-sp@users.noreply.github.com>
2024-12-01 16:10:33 +0700

    add tab component and fix color harmony

--- src/components/color/detail.tsx
+++ src/components/color/detail.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { type ColorConverter, stringifyHsl, stringifyRgb } from "~/lib/utils";
 import type { AnyColor, HarmonyColor } from "~/lib/types";
 import { useColorProvider, useColorStore } from "./provider";
+import { Tabs, TabList, Tab, TabPanel } from "../ui/tabs";
 import Link from "next/link";
 import clsx from "clsx";
 
@@ -31,7 +32,7 @@ function ColorConversion({ hex, color }: { hex: string; color: ColorConverter })
   const luminance = `${Math.round(color.luminance() * 100)}%`;
 
   return (
-    <div className="grid gap-4" role="none">
+    <section aria-label="color detail" className="grid gap-4">
       <div
         role="presentation"
         className="flex h-36 rounded-lg px-4 py-2 text-center text-lg md:h-48 lg:h-60 xl:h-80 max-sm:h-48"
@@ -57,7 +58,7 @@ function ColorConversion({ hex, color }: { hex: string; color: ColorConverter })
         <span className="text-sm font-medium text-holy-400">Luminance</span>
         <code className="font-mono text-base text-holy-100">{luminance}</code>
       </p>
-    </div>
+    </section>
   );
 }
 
@@ -82,73 +83,59 @@ function ColorAnalysis({ rgb, color }: { rgb: string; color: ColorConverter }) {
   const blackLargeAAA = color.isReadable(colorBlack, { level: "AAA", size: "large" });
 
   return (
-    <div className="grid gap-4" role="none">
-      <p
-        className="flex h-36 items-center justify-center rounded-lg px-4 py-2 text-center text-lg md:h-48 lg:h-60 xl:h-80 max-sm:h-48"
-        style={
-          preview === "light"
-            ? { backgroundColor: colorWhite, color: rgb, border: "1px solid" }
-            : { backgroundColor: colorBlack, color: rgb, border: "1px solid" }
-        }
-        tabIndex={0}
-      >
-        {textPreview}
-      </p>
-      <div className="flex flex-wrap gap-2" role="none">
-        <button
-          aria-label="set color on light background"
-          className={clsx(
-            preview === "light"
-              ? "bg-holy-800 text-holy-200"
-              : "border-holy-700 text-holy-400 hover:bg-holy-800 focus:bg-holy-800 active:border-holy-600 active:bg-holy-700",
-            "rounded-md px-2 py-1 text-sm font-normal",
-          )}
-          onClick={() => setPreview("light")}
-        >
-          <span>Light</span>
-        </button>
-        <button
-          aria-label="set color on dark background"
-          className={clsx(
-            preview === "dark"
-              ? "bg-holy-800 text-holy-200"
-              : "border-holy-700 text-holy-400 hover:bg-holy-800 focus:bg-holy-800 active:border-holy-600 active:bg-holy-700",
-            "rounded-md px-2 py-1 text-sm font-normal",
-          )}
-          onClick={() => setPreview("dark")}
-        >
-          <span>Dark</span>
-        </button>
-      </div>
-      <p className="inline-grid" tabIndex={0}>
-        <span className="text-sm font-medium text-holy-400">Contrast Ratio</span>
-        <code className="font-mono text-base text-holy-100">{preview === "light" ? whiteContrast : blackContrast}</code>
-      </p>
-      <p className="inline-grid" tabIndex={0}>
-        <span className="text-sm font-medium text-holy-400">{`WCAG AA (Normal)`}</span>
-        <code className="font-mono text-base text-holy-100">
-          {(preview === "light" ? whiteNormalAA : blackNormalAA) ? "Pass" : "Fail"}
-        </code>
-      </p>
-      <p className="inline-grid" tabIndex={0}>
-        <span className="text-sm font-medium text-holy-400">{`WCAG AAA (Normal)`}</span>
-        <code className="font-mono text-base text-holy-100">
-          {(preview === "light" ? whiteNormalAAA : blackNormalAAA) ? "Pass" : "Fail"}
-        </code>
-      </p>
-      <p className="inline-grid" tabIndex={0}>
-        <span className="text-sm font-medium text-holy-400">{`WCAG AA (Large)`}</span>
-        <code className="font-mono text-base text-holy-100">
-          {(preview === "light" ? whiteLargeAA : blackLargeAA) ? "Pass" : "Fail"}
-        </code>
-      </p>
-      <p className="inline-grid" tabIndex={0}>
-        <span className="text-sm font-medium text-holy-400">{`WCAG AAA (Large)`}</span>
-        <code className="font-mono text-base text-holy-100">
-          {(preview === "light" ? whiteLargeAAA : blackLargeAAA) ? "Pass" : "Fail"}
-        </code>
-      </p>
-    </div>
+    <Tabs label="color analysis" initValue={{ section: "dark" }}>
+      <TabList label="color scheme">
+        <Tab select="light" callback={() => setPreview("light")}>
+          Light
+        </Tab>
+        <Tab select="dark" callback={() => setPreview("dark")}>
+          Dark
+        </Tab>
+      </TabList>
+      <TabPanel>
+        <div className="grid gap-4">
+          <p
+            className="flex h-36 items-center justify-center rounded-lg px-4 py-2 text-center text-lg md:h-48 lg:h-60 xl:h-80 max-sm:h-48"
+            style={
+              preview === "light"
+                ? { backgroundColor: colorWhite, color: rgb, border: "1px solid currentColor" }
+                : { backgroundColor: colorBlack, color: rgb, border: "1px solid currentColor" }
+            }
+            tabIndex={0}
+          >
+            {textPreview}
+          </p>
+          <p className="inline-grid" tabIndex={0}>
+            <span className="text-sm font-medium text-holy-400">Contrast Ratio</span>
+            <code className="font-mono text-base text-holy-100">{preview === "light" ? whiteContrast : blackContrast}</code>
+          </p>
+          <p className="inline-grid" tabIndex={0}>
+            <span className="text-sm font-medium text-holy-400">{`WCAG AA (Normal)`}</span>
+            <code className="font-mono text-base text-holy-100">
+              {(preview === "light" ? whiteNormalAA : blackNormalAA) ? "Pass" : "Fail"}
+            </code>
+          </p>
+          <p className="inline-grid" tabIndex={0}>
+            <span className="text-sm font-medium text-holy-400">{`WCAG AAA (Normal)`}</span>
+            <code className="font-mono text-base text-holy-100">
+              {(preview === "light" ? whiteNormalAAA : blackNormalAAA) ? "Pass" : "Fail"}
+            </code>
+          </p>
+          <p className="inline-grid" tabIndex={0}>
+            <span className="text-sm font-medium text-holy-400">{`WCAG AA (Large)`}</span>
+            <code className="font-mono text-base text-holy-100">
+              {(preview === "light" ? whiteLargeAA : blackLargeAA) ? "Pass" : "Fail"}
+            </code>
+          </p>
+          <p className="inline-grid" tabIndex={0}>
+            <span className="text-sm font-medium text-holy-400">{`WCAG AAA (Large)`}</span>
+            <code className="font-mono text-base text-holy-100">
+              {(preview === "light" ? whiteLargeAAA : blackLargeAAA) ? "Pass" : "Fail"}
+            </code>
+          </p>
+        </div>
+      </TabPanel>
+    </Tabs>
   );
 }
 
@@ -156,24 +143,25 @@ function ColorHarmony({ color, convert }: { color: ColorConverter; convert: (new
   const store = useColorStore((state) => state);
 
   const hsl = color.toHsl();
-  const baseHue = hsl.h;
-
-  const hue = (deg: number) => {
-    const angle = Math.round(deg + baseHue);
-
-    return ((angle % 360) + 360) % 360;
-  };
-
-  const hueShift = (harmonies: number[]) =>
-    harmonies.map((c) => {
-      const newColor = convert({ ...hsl, h: c });
-
-      return { hex: newColor.toHex(), rgb: newColor.toRgb() };
-    });
 
   const [harmonyType, setHarmonyType] = useState<HarmonyColor>(store.harmony);
 
-  const harmony = () => {
+  const harmony = useCallback(() => {
+    const baseHue = hsl.h;
+
+    const hue = (deg: number) => {
+      const angle = Math.round(deg + baseHue);
+
+      return ((angle % 360) + 360) % 360;
+    };
+
+    const hueShift = (harmonies: number[]) =>
+      harmonies.map((c) => {
+        const newColor = convert({ ...hsl, h: c });
+
+        return { hex: newColor.toHex(), rgb: newColor.toRgb() };
+      });
+
     switch (harmonyType) {
       case "complementary":
         return hueShift([baseHue, hue(180)]);
@@ -197,7 +185,9 @@ function ColorHarmony({ color, convert }: { color: ColorConverter; convert: (new
         return hueShift([hue(-30), baseHue, hue(30), hue(150), hue(210)]);
         break;
     }
-  };
+  }, [harmonyType, convert, hsl]);
+
+  const harmonies = useMemo(() => harmony(), [harmony]);
 
   const updateSection = (type: HarmonyColor) => {
     setHarmonyType(type);
@@ -205,123 +195,57 @@ function ColorHarmony({ color, convert }: { color: ColorConverter; convert: (new
   };
 
   return (
-    <aside aria-label="color harmony" className="grid gap-4">
-      <nav aria-label="color combinations" className="flex flex-wrap gap-2">
-        <button
+    <Tabs label="color harmony" initValue={{ section: harmonyType }}>
+      <TabList label="color combinations">
+        <Tab select="complementary" callback={() => updateSection("complementary")}>
+          Complementarty
+        </Tab>
+        <Tab select="analogous" callback={() => updateSection("analogous")}>
+          Analogous
+        </Tab>
+        <Tab select="triadic" callback={() => updateSection("triadic")}>
+          Triadic
+        </Tab>
+        <Tab select="split-complementary" callback={() => updateSection("split-complementary")}>
+          Split Complementary
+        </Tab>
+        <Tab select="tetradic" callback={() => updateSection("tetradic")}>
+          Tetradic
+        </Tab>
+        <Tab select="rectangle" callback={() => updateSection("rectangle")}>
+          Rectangle
+        </Tab>
+        <Tab select="double-split-complementary" callback={() => updateSection("double-split-complementary")}>
+          Double Split Complementary
+        </Tab>
+      </TabList>
+      <TabPanel>
+        <ul
+          aria-label={`${harmonies.length} colors`}
           className={clsx(
-            harmonyType === "complementary"
-              ? "bg-holy-800 text-holy-200"
-              : "border-holy-700 text-holy-400 hover:bg-holy-800 focus:bg-holy-800 active:border-holy-600 active:bg-holy-700",
-            "rounded-md px-2 py-1 text-sm font-normal",
+            "grid gap-4",
+            harmonyType === "complementary" && "sm:grid-cols-2",
+            harmonyType === "analogous" && "sm:grid-cols-3",
+            harmonyType === "triadic" && "sm:grid-cols-3",
+            harmonyType === "split-complementary" && "sm:grid-cols-3",
+            harmonyType === "tetradic" && "sm:grid-cols-4",
+            harmonyType === "rectangle" && "sm:grid-cols-4",
+            harmonyType === "double-split-complementary" && "sm:grid-cols-5",
           )}
-          onClick={() => updateSection("complementary")}
         >
-          <span>Complementary</span>
-        </button>
-        <button
-          className={clsx(
-            harmonyType === "analogous"
-              ? "bg-holy-800 text-holy-200"
-              : "border-holy-700 text-holy-400 hover:bg-holy-800 focus:bg-holy-800 active:border-holy-600 active:bg-holy-700",
-            "rounded-md px-2 py-1 text-sm font-normal",
-          )}
-          onClick={() => updateSection("analogous")}
-        >
-          <span>Analogous</span>
-        </button>
-        <button
-          className={clsx(
-            harmonyType === "triadic"
-              ? "bg-holy-800 text-holy-200"
-              : "border-holy-700 text-holy-400 hover:bg-holy-800 focus:bg-holy-800 active:border-holy-600 active:bg-holy-700",
-            "rounded-md px-2 py-1 text-sm font-normal",
-          )}
-          onClick={() => updateSection("triadic")}
-        >
-          <span>Triadic</span>
-        </button>
-        <button
-          className={clsx(
-            harmonyType === "split-complementary"
-              ? "bg-holy-800 text-holy-200"
-              : "border-holy-700 text-holy-400 hover:bg-holy-800 focus:bg-holy-800 active:border-holy-600 active:bg-holy-700",
-            "rounded-md px-2 py-1 text-sm font-normal",
-          )}
-          onClick={() => updateSection("split-complementary")}
-        >
-          <span>Split Complementary</span>
-        </button>
-        <button
-          className={clsx(
-            harmonyType === "tetradic"
-              ? "bg-holy-800 text-holy-200"
-              : "border-holy-700 text-holy-400 hover:bg-holy-800 focus:bg-holy-800 active:border-holy-600 active:bg-holy-700",
-            "rounded-md px-2 py-1 text-sm font-normal",
-          )}
-          onClick={() => updateSection("tetradic")}
-        >
-          <span>Tetradic</span>
-        </button>
-        <button
-          className={clsx(
-            harmonyType === "rectangle"
-              ? "bg-holy-800 text-holy-200"
-              : "border-holy-700 text-holy-400 hover:bg-holy-800 focus:bg-holy-800 active:border-holy-600 active:bg-holy-700",
-            "rounded-md px-2 py-1 text-sm font-normal",
-          )}
-          onClick={() => updateSection("rectangle")}
-        >
-          <span>Rectangle</span>
-        </button>
-        <button
-          className={clsx(
-            harmonyType === "double-split-complementary"
-              ? "bg-holy-800 text-holy-200"
-              : "border-holy-700 text-holy-400 hover:bg-holy-800 focus:bg-holy-800 active:border-holy-600 active:bg-holy-700",
-            "rounded-md px-2 py-1 text-sm font-normal",
-          )}
-          onClick={() => updateSection("double-split-complementary")}
-        >
-          <span>Double Split Complementary</span>
-        </button>
-      </nav>
-      <ListColorHarmony section={harmonyType} color={harmony()} />
-    </aside>
-  );
-}
-
-function ListColorHarmony({
-  section,
-  color,
-}: {
-  section: string;
-  color: { hex: string; rgb: { r: number; g: number; b: number } }[];
-}) {
-  return (
-    <ul
-      aria-label={section.replaceAll("-", " ")}
-      className={clsx(
-        "grid gap-4",
-        section === "complementary" && "sm:grid-cols-2",
-        section === "analogous" && "sm:grid-cols-3",
-        section === "triadic" && "sm:grid-cols-3",
-        section === "split-complementary" && "sm:grid-cols-3",
-        section === "tetradic" && "sm:grid-cols-4",
-        section === "rectangle" && "sm:grid-cols-4",
-        section === "double-split-complementary" && "sm:grid-cols-5",
-      )}
-    >
-      {color.map((c, i) => (
-        <li key={i} aria-label={c.hex} className="inline-grid">
-          <Link href={`/color/${c.hex.replace("#", "")}`} className="flex rounded-lg" title={c.hex}>
-            <span
-              role="presentation"
-              className="inline-flex h-36 w-full rounded-lg md:h-48 lg:h-60 xl:h-80 max-sm:h-48"
-              style={{ backgroundColor: stringifyRgb(c.rgb) }}
-            ></span>
-          </Link>
-        </li>
-      ))}
-    </ul>
+          {harmonies.map((c, i) => (
+            <li key={i} aria-label={c.hex} className="inline-grid">
+              <Link aria-label={`Color: ${c.hex}`} href={`/color/${c.hex.replace("#", "")}`} className="flex rounded-lg">
+                <span
+                  role="presentation"
+                  className="inline-flex h-36 w-full rounded-lg md:h-48 lg:h-60 xl:h-80 max-sm:h-48"
+                  style={{ backgroundColor: stringifyRgb(c.rgb) }}
+                ></span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </TabPanel>
+    </Tabs>
   );
 }
