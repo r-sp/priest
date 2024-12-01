4c8863f7d9e0b3abc92dd639df48bc8231eeb446
r-sp <36327242+r-sp@users.noreply.github.com>
2024-12-01 22:06:22 +0700

    unified color components

--- src/components/color/index.ts
+++ src/components/color/index.ts
@@ -1,7 +1,10 @@
-import { ColorProvider, useColorProvider } from "./provider";
+import { ColorProvider, useColorStore, useColorProvider } from "./provider";
 
 import ColorDetail from "./detail";
 import ColorFeed from "./feed";
 import ColorSlider from "./slider";
 
-export { useColorProvider, ColorDetail, ColorFeed, ColorProvider, ColorSlider };
+import ColorInput from "./input";
+import ColorRandom from "./random";
+
+export { useColorStore, useColorProvider, ColorDetail, ColorFeed, ColorProvider, ColorSlider, ColorInput, ColorRandom };
