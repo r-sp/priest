ca9d4554ceb20fcc52325e4d4d1eb42b06c70ec0
r-sp <36327242+r-sp@users.noreply.github.com>
2024-12-04 17:04:19 +0700

    add intro page

---
+++ src/components/ui/intro.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+export default function Intro() {
+  return (
+    <article className="relative flex min-h-svh items-end justify-center px-4 py-10">
+      <header className="text-center">
+        <h1 className="text-2xl font-semibold text-neutral-900 dark:text-neutral-100">
+          Priest
+        </h1>
+        <p className="text-base font-normal text-neutral-600 dark:text-neutral-400">
+          <Link href="/playground" className="text-inherit">
+            The Holy Colors
+          </Link>
+        </p>
+      </header>
+      <div
+        role="presentation"
+        className="pointer-events-none absolute top-0 right-0 left-0 grid min-h-svh pb-32"
+        tabIndex={-1}
+      >
+        <div className="bg-red-400"></div>
+        <div className="bg-orange-400"></div>
+        <div className="bg-amber-400"></div>
+        <div className="bg-yellow-400"></div>
+        <div className="bg-lime-400"></div>
+        <div className="bg-green-400"></div>
+        <div className="bg-emerald-400"></div>
+        <div className="bg-teal-400"></div>
+        <div className="bg-cyan-400"></div>
+        <div className="bg-sky-400"></div>
+        <div className="bg-blue-400"></div>
+        <div className="bg-indigo-400"></div>
+        <div className="bg-violet-400"></div>
+        <div className="bg-purple-400"></div>
+        <div className="bg-fuchsia-400"></div>
+        <div className="bg-pink-400"></div>
+        <div className="bg-rose-400"></div>
+      </div>
+    </article>
+  );
+}
