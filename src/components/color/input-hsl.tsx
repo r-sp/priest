c446283fd0e8e27855260d866f0a2fa06aab94b0
r-sp <36327242+r-sp@users.noreply.github.com>
2024-12-04 23:29:10 +0700

    add color slider based on each color space

--- src/components/color/input-hsl.tsx
+++ src/components/color/input-hsl.tsx
@@ -1,27 +1,36 @@
 "use client";
 
 import { useState } from "react";
-import { hsl, formatHsl, formatHex } from "culori";
+import { Hsl, Css, Hex } from "~/lib/color";
 
 export default function InputHsl() {
   const [color, setColor] = useState({ h: 119.5, s: 0.7595, l: 0.6902 });
 
-  const input = hsl({ mode: "hsl", ...color });
-  const css = formatHsl(input);
-  const hex = formatHex(css);
+  const input = Hsl(color);
+  const css = Css(input);
+  const hex = Hex(css);
+
+  const trackSaturationLeft = Css(Hsl({ ...color, s: 0 }));
+  const trackSaturationRight = Css(Hsl({ ...color, s: 1 }));
+  const trackLightnessCenter = Css(Hsl({ ...color, s: 1, l: 0.5 }));
 
   return (
     <section
       aria-labelledby="color-hsl"
-      className="mx-auto grid w-full max-w-3xl gap-4 border-t border-t-neutral-400 pt-8"
+      className="mx-auto grid w-full max-w-3xl gap-4 border-t border-t-neutral-400 pt-8 dark:border-t-neutral-700"
     >
       <h2 id="color-hsl">
         <code>
-          <span className="text-neutral-700">{css}</span> | <span>{hex}</span>
+          <span className="text-neutral-700 dark:text-neutral-300">{css}</span>{" "}
+          | <span>{hex}</span>
         </code>
       </h2>
-      <div className="inline-grid h-64" style={{ backgroundColor: css }}></div>
-      <div className="inline-grid">
+      <span
+        role="presentation"
+        className="h-svh-1/2 inline-grid"
+        style={{ backgroundColor: css }}
+      ></span>
+      <div className="relative inline-grid">
         <input
           type="range"
           id="hsl-hue"
@@ -29,10 +38,19 @@ export default function InputHsl() {
           max={360}
           step={0.01}
           value={color.h}
+          className="color-slider relative z-2 text-neutral-400"
           onChange={(e) => setColor({ ...color, h: e.target.valueAsNumber })}
         />
+        <span
+          role="presentation"
+          className="pointer-events-none absolute top-1 right-0 bottom-1 left-0 z-0 rounded-md"
+          style={{
+            backgroundImage:
+              "linear-gradient(to right, rgb(255, 0, 0) 0%, rgb(255, 255, 0) 17%, rgb(0, 255, 0) 33%, rgb(0, 255, 255) 50%, rgb(0, 0, 255) 67%, rgb(255, 0, 255) 83%, rgb(255, 0, 0) 100%)",
+          }}
+        ></span>
       </div>
-      <div className="inline-grid">
+      <div className="relative inline-grid">
         <input
           type="range"
           id="hsl-saturation"
@@ -40,10 +58,18 @@ export default function InputHsl() {
           max={1}
           step={0.0001}
           value={color.s}
+          className="color-slider relative z-2 text-neutral-400"
           onChange={(e) => setColor({ ...color, s: e.target.valueAsNumber })}
         />
+        <span
+          role="presentation"
+          className="pointer-events-none absolute top-1 right-0 bottom-1 left-0 z-0 rounded-md"
+          style={{
+            backgroundImage: `linear-gradient(to right, ${trackSaturationLeft}, ${trackSaturationRight})`,
+          }}
+        ></span>
       </div>
-      <div className="inline-grid">
+      <div className="relative inline-grid">
         <input
           type="range"
           id="hsl-lightness"
@@ -51,8 +77,16 @@ export default function InputHsl() {
           max={1}
           step={0.0001}
           value={color.l}
+          className="color-slider relative z-2 text-neutral-400"
           onChange={(e) => setColor({ ...color, l: e.target.valueAsNumber })}
         />
+        <span
+          role="presentation"
+          className="pointer-events-none absolute top-1 right-0 bottom-1 left-0 z-0 rounded-md"
+          style={{
+            backgroundImage: `linear-gradient(to right, rgb(0,0,0), ${trackLightnessCenter}, rgb(255, 255, 255))`,
+          }}
+        ></span>
       </div>
     </section>
   );
--- src/components/color/input-oklch.tsx
+++ src/components/color/input-oklch.tsx
@@ -1,27 +1,44 @@
 "use client";
 
 import { useState } from "react";
-import { oklch, formatCss, formatHex } from "culori";
+import { Oklch, Css, Hex } from "~/lib/color";
 
 export default function InputOklch() {
   const [color, setColor] = useState({ l: 0.85, c: 0.19, h: 143.48 });
 
-  const format = oklch({ mode: "oklch", ...color });
-  const css = formatCss(format);
-  const hex = formatHex(css);
+  const format = Oklch(color);
+  const css = Css(format);
+  const hex = Hex(css);
+
+  const trackLightnessLeft = Css(Oklch({ ...color, l: 0 }));
+  const trackLightnessRight = Css(Oklch({ ...color, l: 1 }));
+  const trackChromaLeft = Css(Oklch({ ...color, c: 0 }));
+  const trackChromaRight = Css(Oklch({ ...color, c: 0.4 }));
+  const trackHueLeft = Css(Oklch({ ...color, h: 0 }));
+  const trackHueRed = Css(Oklch({ ...color, h: 60 }));
+  const trackHueGreen = Css(Oklch({ ...color, h: 120 }));
+  const trackHueCenter = Css(Oklch({ ...color, h: 180 }));
+  const trackHueBlue = Css(Oklch({ ...color, h: 240 }));
+  const trackHuePurple = Css(Oklch({ ...color, h: 300 }));
+  const trackHueRight = Css(Oklch({ ...color, h: 360 }));
 
   return (
     <section
       aria-labelledby="color-oklch"
-      className="mx-auto grid w-full max-w-3xl gap-4 border-t border-t-neutral-400 pt-8"
+      className="mx-auto grid w-full max-w-3xl gap-4 border-t border-t-neutral-400 pt-8 dark:border-t-neutral-700"
     >
       <h2 id="color-oklch">
         <code>
-          <span className="text-neutral-700">{css}</span> | <span>{hex}</span>
+          <span className="text-neutral-700 dark:text-neutral-300">{css}</span>{" "}
+          | <span>{hex}</span>
         </code>
       </h2>
-      <div className="inline-grid h-64" style={{ backgroundColor: css }}></div>
-      <div className="inline-grid">
+      <span
+        role="presentation"
+        className="h-svh-1/2 inline-grid"
+        style={{ backgroundColor: css }}
+      ></span>
+      <div className="relative inline-grid">
         <input
           type="range"
           id="oklch-lightness"
@@ -29,10 +46,18 @@ export default function InputOklch() {
           max={1}
           step={0.001}
           value={color.l}
+          className="color-slider relative z-2 text-neutral-400"
           onChange={(e) => setColor({ ...color, l: e.target.valueAsNumber })}
         />
+        <span
+          role="presentation"
+          className="pointer-events-none absolute top-1 right-0 bottom-1 left-0 z-0 rounded-md"
+          style={{
+            backgroundImage: `linear-gradient(to right, ${trackLightnessLeft}, ${trackLightnessRight})`,
+          }}
+        ></span>
       </div>
-      <div className="inline-grid">
+      <div className="relative inline-grid">
         <input
           type="range"
           id="oklch-chroma"
@@ -40,10 +65,18 @@ export default function InputOklch() {
           max={0.4}
           step={0.001}
           value={color.c}
+          className="color-slider relative z-2 text-neutral-400"
           onChange={(e) => setColor({ ...color, c: e.target.valueAsNumber })}
         />
+        <span
+          role="presentation"
+          className="pointer-events-none absolute top-1 right-0 bottom-1 left-0 z-0 rounded-md"
+          style={{
+            backgroundImage: `linear-gradient(to right, ${trackChromaLeft}, ${trackChromaRight})`,
+          }}
+        ></span>
       </div>
-      <div className="inline-grid">
+      <div className="relative inline-grid">
         <input
           type="range"
           id="oklch-hue"
@@ -51,8 +84,16 @@ export default function InputOklch() {
           max={360}
           step={0.01}
           value={color.h}
+          className="color-slider relative z-2 text-neutral-400"
           onChange={(e) => setColor({ ...color, h: e.target.valueAsNumber })}
         />
+        <span
+          role="presentation"
+          className="pointer-events-none absolute top-1 right-0 bottom-1 left-0 z-0 rounded-md"
+          style={{
+            backgroundImage: `linear-gradient(to right, ${trackHueLeft}, ${trackHueRed}, ${trackHueGreen}, ${trackHueCenter}, ${trackHueBlue}, ${trackHuePurple}, ${trackHueRight})`,
+          }}
+        ></span>
       </div>
     </section>
   );
