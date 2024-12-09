eab0b50417c1c1a8514e041cf39f51d4ad7f08de
r-sp <36327242+r-sp@users.noreply.github.com>
2024-12-09 14:31:57 +0700

    remove hex props

--- src/app/color/[hex]/page.tsx
+++ src/app/color/[hex]/page.tsx
@@ -25,7 +25,7 @@ export default async function ColorHexPage({
   const valid = isValidColor(`#${hex}`);
 
   if (valid) {
-    return <ColorDetail hex={hex} />;
+    return <ColorDetail />;
   } else {
     return permanentRedirect("/color");
   }
