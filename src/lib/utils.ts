4792f7bb7c861fc62bce6ee940e63d55d371931e
r-sp <36327242+r-sp@users.noreply.github.com>
2024-12-10 13:57:40 +0700

    fix limiter number with range no round

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -14,12 +14,7 @@ export const floor = (
   return Math.floor(base * number) / base + 0;
 };
 
-export const limiterAngle = (
-  value: number,
-  min: number,
-  max: number,
-): number => {
+export const limiter = (value: number, min: number, max: number): number => {
   const range = max - min;
-  const angle = ((((value - min) % range) + range) % range) + min;
-  return round(angle, 4);
+  return ((((value - min) % range) + range) % range) + min;
 };
