cb2deee7f62a2bcd4d663ab503c12c71371be7b5
r-sp <36327242+r-sp@users.noreply.github.com>
2024-12-13 21:34:56 +0700

    add an index of components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import { ColorProvider } from "./color/provider";
+
+import ThemeScript from "./theme/script";
+import Header from "./header";
+import Footer from "./footer";
+
+export { ColorProvider, ThemeScript, Header, Footer };
