a9a33ed66c8cb6212dd5545a8525dcf613ee7be4
r-sp <36327242+r-sp@users.noreply.github.com>
2024-12-17 13:28:56 +0700

    simplify color detail

--- src/components/color/detail.tsx
+++ src/components/color/detail.tsx
@@ -1,16 +1,23 @@
 "use client";
 
+import {
+  createColor,
+  brightness,
+  luminance,
+  contrast,
+  isReadable,
+  isValidHex,
+} from "~/lib/color";
+import { useState } from "react";
 import { useParams } from "next/navigation";
 import { useColorStore } from "./provider";
-import { createColor, isValidHex } from "~/lib/color";
-import { ColorContrast, ColorSlider } from "~/components/color";
-import clsx from "clsx";
+import Link from "next/link";
 
 export default function ColorDetail() {
   const colorBy = useParams<{ hex: string }>();
   const store = useColorStore((state) => state);
 
-  const { hex, rgb, hsl, hwb, lab, lch, oklab, oklch, mode } = colorBy.hex
+  const { hex, rgb, hsl, hwb, lab, lch, oklab, oklch } = colorBy.hex
     ? createColor(isValidHex(colorBy.hex))
     : store;
 
@@ -23,93 +30,115 @@ export default function ColorDetail() {
   const colorOklab = oklab.css;
   const colorOklch = oklch.css;
 
-  const modeRgb = mode === "rgb";
-  const modeHsl = mode === "hsl";
-  const modeHwb = mode === "hwb";
-  const modeLab = mode === "lab";
-  const modeLch = mode === "lch";
-  const modeOklab = mode === "oklab";
-  const modeOklch = mode === "oklch";
+  const check = rgb.color;
+  const colorBrightness = brightness(check);
+  const colorLuminance = luminance(check);
+
+  const [isDark, setIsDark] = useState<boolean>(false);
+
+  const colorWhite = { r: 1, g: 1, b: 1 };
+  const whiteContrast = contrast(check, colorWhite);
+  const whiteNormalAA = isReadable(check, colorWhite, {
+    level: "AA",
+    size: "normal",
+  })
+    ? "Pass"
+    : "Fail";
+  const whiteNormalAAA = isReadable(check, colorWhite, {
+    level: "AAA",
+    size: "normal",
+  })
+    ? "Pass"
+    : "Fail";
+  const whiteLargeAA = isReadable(check, colorWhite, {
+    level: "AA",
+    size: "large",
+  })
+    ? "Pass"
+    : "Fail";
+  const whiteLargeAAA = isReadable(check, colorWhite, {
+    level: "AAA",
+    size: "large",
+  })
+    ? "Pass"
+    : "Fail";
+
+  const colorBlack = { r: 0, g: 0, b: 0 };
+  const blackContrast = contrast(check, colorBlack);
+  const blackNormalAA = isReadable(check, colorBlack, {
+    level: "AA",
+    size: "normal",
+  })
+    ? "Pass"
+    : "Fail";
+  const blackNormalAAA = isReadable(check, colorBlack, {
+    level: "AAA",
+    size: "normal",
+  })
+    ? "Pass"
+    : "Fail";
+  const blackLargeAA = isReadable(check, colorBlack, {
+    level: "AA",
+    size: "large",
+  })
+    ? "Pass"
+    : "Fail";
+  const blackLargeAAA = isReadable(check, colorBlack, {
+    level: "AAA",
+    size: "large",
+  })
+    ? "Pass"
+    : "Fail";
 
   return (
-    <article
-      className={clsx(
-        "grid gap-x-4 gap-y-8 max-xl:px-3",
-        !colorBy.hex && "mx-auto max-w-7xl md:grid-cols-2",
-      )}
-    >
-      <header
-        className={clsx(
-          "mx-auto w-full max-w-3xl",
-          !colorBy.hex && "md:relative",
-        )}
-      >
-        <div role="none" className={clsx(!colorBy.hex && "md:sticky md:top-4")}>
-          <div role="presentation" className="frame rounded-lg">
-            <span style={{ backgroundColor: colorRgb }}></span>
-          </div>
-          <h1 className="mt-4 font-mono">
-            <code>{colorHex}</code>
-          </h1>
-          <p
-            className={clsx(
-              modeRgb && "text-neutral-800 dark:text-neutral-200",
-              "font-mono",
-            )}
-          >
-            <code>{colorRgb}</code>
-          </p>
-          <p
-            className={clsx(
-              modeHsl && "text-neutral-800 dark:text-neutral-200",
-              "font-mono",
-            )}
-          >
-            <code>{colorHsl}</code>
-          </p>
-          <p
-            className={clsx(
-              modeHwb && "text-neutral-800 dark:text-neutral-200",
-              "font-mono",
-            )}
-          >
-            <code>{colorHwb}</code>
-          </p>
-          <p
-            className={clsx(
-              modeLch && "text-neutral-800 dark:text-neutral-200",
-              "mt-4 font-mono",
-            )}
-          >
-            <code>{colorLch}</code>
-          </p>
-          <p
-            className={clsx(
-              modeOklch && "text-neutral-800 dark:text-neutral-200",
-            )}
-          >
-            <code>{colorOklch}</code>
-          </p>
-          <p
-            className={clsx(
-              modeLab && "text-neutral-800 dark:text-neutral-200",
-              "font-mono",
-            )}
-          >
-            <code>{colorLab}</code>
-          </p>
-          <p
-            className={clsx(
-              modeOklab && "text-neutral-800 dark:text-neutral-200",
-              "font-mono",
-            )}
-          >
-            <code>{colorOklab}</code>
-          </p>
-          <ColorContrast rgb={rgb.color} />
-        </div>
-      </header>
-      {!colorBy.hex && <ColorSlider state={store} />}
-    </article>
+    <div className="grid gap-x-4 gap-y-8 px-3 py-4">
+      <article className="mx-auto w-full max-w-3xl">
+        <Link
+          aria-label={`view color ${hex}`}
+          href={`/color/${hex.replace("#", "")}/img`}
+          className="frame inline-grid w-full rounded-lg"
+        >
+          <span style={{ backgroundColor: colorRgb }}></span>
+        </Link>
+        <h1 className="mt-4">
+          <code>{colorHex}</code>
+        </h1>
+        <p>
+          <code>{colorRgb}</code>
+        </p>
+        <p>
+          <code>{colorHsl}</code>
+        </p>
+        <p>
+          <code>{colorHwb}</code>
+        </p>
+        <p className="mt-4">
+          <code>{colorLch}</code>
+        </p>
+        <p>
+          <code>{colorOklch}</code>
+        </p>
+        <p>
+          <code>{colorLab}</code>
+        </p>
+        <p>
+          <code>{colorOklab}</code>
+        </p>
+        <p className="mt-4">Brightness: {colorBrightness}</p>
+        <p>Luminance: {colorLuminance}</p>
+        <button
+          aria-label={`change contrast background color to ${isDark ? "white" : "black"}`}
+          className="mt-4 text-neutral-800 dark:text-neutral-200"
+          onClick={() => setIsDark(!isDark)}
+        >
+          {`Color on ${isDark ? "Black" : "White"}`}
+        </button>
+        <p>{`Contrast: ${isDark ? blackContrast : whiteContrast}`}</p>
+        <p className="mt-2">{`Normal AA: ${isDark ? blackNormalAA : whiteNormalAA}`}</p>
+        <p>{`Normal AAA: ${isDark ? blackNormalAAA : whiteNormalAAA}`}</p>
+        <p className="mt-2">{`Large AA: ${isDark ? blackLargeAA : whiteLargeAA}`}</p>
+        <p>{`Large AAA: ${isDark ? blackLargeAAA : whiteLargeAAA}`}</p>
+      </article>
+    </div>
   );
 }
