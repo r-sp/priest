679b245e94262010c3bd286df152900c270c4d83
r-sp <36327242+r-sp@users.noreply.github.com>
2024-12-18 23:16:26 +0700

    remove unused components

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-import { ColorProvider } from "./color/provider";
-
-import ThemeScript from "./theme/script";
-import Header from "./header";
-import Footer from "./footer";
-
-export { ColorProvider, ThemeScript, Header, Footer };
