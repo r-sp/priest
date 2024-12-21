f1d867ba65668d53d49a9a97cf41f4a470b6a6c9
r-sp <36327242+r-sp@users.noreply.github.com>
2024-12-21 22:11:46 +0700

    update color format with new color modes

--- src/lib/format.ts
+++ src/lib/format.ts
@@ -6,8 +6,10 @@ import type {
   LchColor,
   OklabColor,
   OklchColor,
+  AnyColorMode,
+  ColorFormat,
+  ColorSpace,
 } from "./color";
-
 import { round } from "./utils";
 
 export const formatHex = (newColor: RgbColor): string => {
@@ -86,4 +88,111 @@ export const formatOklch = (newColor: OklchColor): string => {
   const hue = round(h || 0, 2);
 
   return `oklch(${lightness} ${chroma} ${hue})`;
+};
+
+export const formatPathMode = (color: AnyColorMode): string => {
+  switch (color.mode) {
+    case "rgb": {
+      const { r, g, b } = color;
+      return `/color?mode=rgb&r=${r}&g=${g}&b=${b}`;
+      break;
+    }
+    case "hsl": {
+      const { h, s, l } = color;
+      return `/color?mode=hsl&h=${h}&s=${s}&l=${l}`;
+      break;
+    }
+    case "hwb": {
+      const { h, w, b } = color;
+      return `/color?mode=hwb&h=${h}&w=${w}&b=${b}`;
+      break;
+    }
+    case "lab": {
+      const { l, a, b } = color;
+      return `/color?mode=lab&l=${l}&a=${a}&b=${b}`;
+      break;
+    }
+    case "lch": {
+      const { l, c, h } = color;
+      return `/color?mode=lch&l=${l}&c=${c}&h=${h}`;
+      break;
+    }
+    case "oklab": {
+      const { l, a, b } = color;
+      return `/color?mode=oklab&l=${l}&a=${a}&b=${b}`;
+      break;
+    }
+    case "oklch": {
+      const { l, c, h } = color;
+      return `/color?mode=oklch&l=${l}&c=${c}&h=${h}`;
+      break;
+    }
+  }
+};
+
+export const formatColorMode = (
+  type: ColorFormat,
+  color: ColorSpace,
+): AnyColorMode => {
+  switch (type) {
+    case "rgb": {
+      return { mode: type, ...color.rgb };
+      break;
+    }
+    case "hsl": {
+      return { mode: type, ...color.hsl };
+      break;
+    }
+    case "hwb": {
+      return { mode: type, ...color.hwb };
+      break;
+    }
+    case "lab": {
+      return { mode: type, ...color.lab };
+      break;
+    }
+    case "lch": {
+      return { mode: type, ...color.lch };
+      break;
+    }
+    case "oklab": {
+      return { mode: type, ...color.oklab };
+      break;
+    }
+    case "oklch": {
+      return { mode: type, ...color.oklch };
+    }
+  }
+};
+
+export const formatCssMode = (type: ColorFormat, color: ColorSpace): string => {
+  switch (type) {
+    case "rgb": {
+      return formatRgb(color.rgb);
+      break;
+    }
+    case "hsl": {
+      return formatHsl(color.hsl);
+      break;
+    }
+    case "hwb": {
+      return formatHwb(color.hwb);
+      break;
+    }
+    case "lab": {
+      return formatLab(color.lab);
+      break;
+    }
+    case "lch": {
+      return formatLch(color.lch);
+      break;
+    }
+    case "oklab": {
+      return formatOklab(color.oklab);
+      break;
+    }
+    case "oklch": {
+      return formatOklch(color.oklch);
+    }
+  }
 };
