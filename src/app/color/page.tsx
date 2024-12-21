810d196a19b2d58fc881169f41b60e710542249d
r-sp <36327242+r-sp@users.noreply.github.com>
2024-12-21 22:00:05 +0700

    add skeleton for color page

--- src/app/color/page.tsx
+++ src/app/color/page.tsx
@@ -1,10 +1,11 @@
 import { type Metadata } from "next";
+import { createColor, initColor } from "~/lib/color";
 import { Suspense } from "react";
-import { createColor, getTodayColor } from "~/lib/color";
+import Wrapper from "~/components/ui/wrapper";
 import Color from "~/components/ui/color";
 
 export function generateMetadata(): Metadata {
-  const color = createColor(getTodayColor());
+  const color = createColor(initColor());
   const link = "https://priest.vercel.app/color";
 
   return {
@@ -25,8 +26,38 @@ export function generateMetadata(): Metadata {
 
 export default function ColorPage() {
   return (
-    <Suspense>
+    <Suspense fallback={<Skeleton />}>
       <Color />
     </Suspense>
+  );
+}
+
+function Skeleton() {
+  return (
+    <Wrapper
+      as="div"
+      maxWidth="1024"
+      className="grid gap-y-6"
+      outerStyle="py-4"
+    >
+      <div className="inline-grid gap-y-3">
+        <div className="frame rounded-lg">
+          <span className="bg-neutral-200 dark:bg-neutral-900"></span>
+        </div>
+        <span className="h-8 w-64 bg-neutral-200 dark:bg-neutral-900"></span>
+      </div>
+      <div className="inline-grid gap-y-2">
+        <span className="h-4 w-48 bg-neutral-200 dark:bg-neutral-900"></span>
+        <span className="h-4 w-56 bg-neutral-200 dark:bg-neutral-900"></span>
+        <span className="h-4 w-56 bg-neutral-200 dark:bg-neutral-900"></span>
+        <span className="h-4 w-56 bg-neutral-200 dark:bg-neutral-900"></span>
+      </div>
+      <div className="inline-grid gap-y-2">
+        <span className="h-4 w-48 bg-neutral-200 dark:bg-neutral-900"></span>
+        <span className="h-4 w-56 bg-neutral-200 dark:bg-neutral-900"></span>
+        <span className="h-4 w-56 bg-neutral-200 dark:bg-neutral-900"></span>
+        <span className="h-4 w-56 bg-neutral-200 dark:bg-neutral-900"></span>
+      </div>
+    </Wrapper>
   );
 }
