b52d2ed439ece24104647f07e2d4b3dcb4962464
r-sp <36327242+r-sp@users.noreply.github.com>
2024-12-22 15:42:43 +0700

    add theme switcher skeleton

--- src/components/ui/theme.tsx
+++ src/components/ui/theme.tsx
@@ -4,6 +4,9 @@ import dynamic from "next/dynamic";
 
 const ThemeSwitcher = dynamic(() => import("../theme/switcher"), {
   ssr: false,
+  loading: () => (
+    <div className="h-8 w-24 rounded-2xl bg-neutral-200 dark:bg-neutral-900"></div>
+  ),
 });
 
 export default function Theme() {
