460db51b353d35ff94413d4d25fd380589ad0adb
r-sp <36327242+r-sp@users.noreply.github.com>
2024-12-22 22:07:40 +0700

    add parse color query and path

--- src/components/color/query.ts
+++ src/components/color/query.ts
@@ -1,4 +1,4 @@
-import { type AnyColorMode } from "~/lib/color";
+import type { AnyColorMode, ColorFormat, ColorSpace } from "~/lib/color";
 import { useQueryStates, parseAsString, parseAsFloat } from "nuqs";
 import {
   formatRgb,
@@ -141,3 +141,73 @@ export function useColorQuery(): { color: AnyColorMode; css: string } | null {
     return null;
   }
 }
+
+export type ColorQuery = {
+  mode: ColorFormat;
+  r?: number;
+  g?: number;
+  b?: number;
+  h?: number;
+  s?: number;
+  l?: number;
+  w?: number;
+  a?: number;
+  c?: number;
+};
+
+export const parseColorQuery = (color: ColorQuery): ColorSpace => {
+  const { mode, r, g, b, h, s, l, w, a, c } = color;
+
+  return {
+    rgb:
+      mode === "rgb" && r !== null && g !== null && b !== null
+        ? { r: r!, g: g!, b: b! }
+        : { r: 0, g: 0, b: 0 },
+    hsl:
+      mode === "hsl" && h !== null && s !== null && l !== null
+        ? { h: h!, s: s!, l: l! }
+        : { h: 0, s: 0, l: 0 },
+    hwb:
+      mode === "hwb" && h !== null && w !== null && b !== null
+        ? { h: h!, w: w!, b: b! }
+        : { h: 0, w: 0, b: 0 },
+    lab:
+      mode === "lab" && l !== null && a !== null && b !== null
+        ? { l: l!, a: a!, b: b! }
+        : { l: 0, a: 0, b: 0 },
+    lch:
+      mode === "lch" && l !== null && c !== null && h !== null
+        ? { l: l!, c: c!, h: h! }
+        : { l: 0, c: 0, h: 0 },
+    oklab:
+      mode === "oklab" && l !== null && a !== null && b !== null
+        ? { l: l!, a: a!, b: b! }
+        : { l: 0, a: 0, b: 0 },
+    oklch:
+      mode === "oklch" && l !== null && c !== null && h !== null
+        ? { l: l!, c: c!, h: h! }
+        : { l: 0, c: 0, h: 0 },
+  };
+};
+
+export const parseColorPath = (color: ColorQuery): string => {
+  const { mode, r, g, b, h, s, l, w, a, c } = color;
+
+  if (mode === "rgb" && r !== null && g !== null && b !== null) {
+    return `color?mode=rgb&r=${r!}&g=${g!}&b=${b!}`;
+  } else if (mode === "hsl" && h !== null && s !== null && l !== null) {
+    return `color?mode=hsl&h=${h!}&s=${s!}&l=${l!}`;
+  } else if (mode === "hwb" && h !== null && w !== null && b !== null) {
+    return `color?mode=hwb&h=${h!}&w=${w!}&b=${b!}`;
+  } else if (mode === "lab" && l !== null && a !== null && b !== null) {
+    return `color?mode=lab&l=${l!}&a=${a!}&b=${b!}`;
+  } else if (mode === "lch" && l !== null && c !== null && h !== null) {
+    return `color?mode=lch&l=${l!}&c=${c!}&h=${h!}`;
+  } else if (mode === "oklab" && l !== null && a !== null && b !== null) {
+    return `color?mode=oklab&l=${l!}&a=${a!}&b=${b!}`;
+  } else if (mode === "oklch" && l !== null && c !== null && h !== null) {
+    return `color?mode=oklch&l=${l!}&c=${c!}&h=${h!}`;
+  } else {
+    return "color";
+  }
+};
