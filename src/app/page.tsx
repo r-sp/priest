de879e710915e687c16bce11dbaae5d364bece37
r-sp <36327242+r-sp@users.noreply.github.com>
2025-01-03 21:48:37 +0700

    fix landing page styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,88 +16,92 @@ export function generateMetadata(): Metadata {
 
 export default function HomePage() {
   return (
-    <Wrapper as="article" className="grid gap-y-4" outerStyle="py-4">
-      <h1
-        id="intro"
-        className="mx-auto max-w-3xl text-center text-3xl font-bold text-neutral-800 dark:text-neutral-200"
-      >
-        Priest: The Holy Colors
-      </h1>
-      <p className="mx-auto max-w-2xl text-center text-lg">
-        Use HEX, RGB, HSL, HWB, plus advanced LAB, LCH, OKLAB & OKLCH color
-        spaces for precise color control and conversions.
-      </p>
-      <Link
-        aria-label="Try Color Playground"
-        href="/color"
-        className="mt-4 inline-grid w-full overflow-hidden rounded-lg"
-      >
-        <span className="h-6 bg-red-400"></span>
-        <span className="h-6 bg-orange-400"></span>
-        <span className="h-6 bg-amber-400"></span>
-        <span className="h-6 bg-yellow-400"></span>
-        <span className="h-6 bg-lime-400"></span>
-        <span className="h-6 bg-green-400"></span>
-        <span className="h-6 bg-emerald-400"></span>
-        <span className="h-6 bg-teal-400"></span>
-        <span className="h-6 bg-cyan-400"></span>
-        <span className="h-6 bg-sky-400"></span>
-        <span className="h-6 bg-blue-400"></span>
-        <span className="h-6 bg-indigo-400"></span>
-        <span className="h-6 bg-violet-400"></span>
-        <span className="h-6 bg-purple-400"></span>
-        <span className="h-6 bg-fuchsia-400"></span>
-        <span className="h-6 bg-pink-400"></span>
-        <span className="h-6 bg-rose-400"></span>
-      </Link>
-      <ul className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
-        <li className="inline-grid">
-          <Link
-            href="/color-harmony"
-            className="action grid content-baseline gap-y-1 rounded-lg border border-neutral-400 px-3 py-4 dark:border-neutral-700"
-          >
-            <h2 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">
-              Color Harmony
-            </h2>
-            <p className="text-sm text-neutral-500 dark:text-neutral-500">
-              Refers to the arrangement of colors that are pleasing to the eye,
-              creating a harmonious color combination that is balanced and
-              aesthetically appealing.
-            </p>
-          </Link>
-        </li>
-        <li className="inline-grid">
-          <Link
-            href="/color-palettes"
-            className="action grid content-baseline gap-y-1 rounded-lg border border-neutral-400 px-3 py-4 dark:border-neutral-700"
-          >
-            <h2 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">
-              Color Palettes
-            </h2>
-            <p className="text-sm text-neutral-500 dark:text-neutral-500">
-              Carefully chosen sets of colors used to evoke specific emotions,
-              establish brand identity, or create a particular aesthetic in
-              visual designs.
-            </p>
-          </Link>
-        </li>
-        <li className="inline-grid">
-          <Link
-            href="/color-shades"
-            className="action grid content-baseline gap-y-1 rounded-lg border border-neutral-400 px-3 py-4 dark:border-neutral-700"
-          >
-            <h2 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">
-              Color Shades
-            </h2>
-            <p className="text-sm text-neutral-500 dark:text-neutral-500">
-              Encompass the spectrum of that color&apos;s lightness and
-              darkness, creating a gradient from its purest form to its darkest
-              possible version.
-            </p>
-          </Link>
-        </li>
-      </ul>
-      <Separator className="my-6" />
+    <Wrapper as="article" className="flex flex-col gap-y-8" outerStyle="py-4">
+      <header className="mx-auto grid w-full max-w-3xl gap-y-3">
+        <h1
+          id="intro"
+          className="text-center text-3xl font-bold text-neutral-800 dark:text-neutral-200"
+        >
+          Priest: The Holy Colors
+        </h1>
+        <p className="mx-auto max-w-2xl text-center">
+          Use HEX, RGB, HSL, HWB, plus advanced LAB, LCH, OKLAB & OKLCH color
+          spaces for precise color control and conversions.
+        </p>
+      </header>
+      <section aria-label="color tools" className="grid gap-y-4">
+        <Link
+          aria-label="Try Color Playground"
+          href="/color"
+          className="grid w-full overflow-hidden rounded-lg"
+        >
+          <span className="h-6 bg-red-400"></span>
+          <span className="h-6 bg-orange-400"></span>
+          <span className="h-6 bg-amber-400"></span>
+          <span className="h-6 bg-yellow-400"></span>
+          <span className="h-6 bg-lime-400"></span>
+          <span className="h-6 bg-green-400"></span>
+          <span className="h-6 bg-emerald-400"></span>
+          <span className="h-6 bg-teal-400"></span>
+          <span className="h-6 bg-cyan-400"></span>
+          <span className="h-6 bg-sky-400"></span>
+          <span className="h-6 bg-blue-400"></span>
+          <span className="h-6 bg-indigo-400"></span>
+          <span className="h-6 bg-violet-400"></span>
+          <span className="h-6 bg-purple-400"></span>
+          <span className="h-6 bg-fuchsia-400"></span>
+          <span className="h-6 bg-pink-400"></span>
+          <span className="h-6 bg-rose-400"></span>
+        </Link>
+        <ul className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
+          <li className="inline-grid">
+            <Link
+              href="/color-harmony"
+              className="action grid content-baseline gap-y-1 rounded-lg border border-neutral-400 px-3 py-4 dark:border-neutral-700"
+            >
+              <h2 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">
+                Color Harmony
+              </h2>
+              <p className="text-sm text-neutral-500 dark:text-neutral-500">
+                Refers to the arrangement of colors that are pleasing to the
+                eye, creating a harmonious color combination that is balanced
+                and aesthetically appealing.
+              </p>
+            </Link>
+          </li>
+          <li className="inline-grid">
+            <Link
+              href="/color-palettes"
+              className="action grid content-baseline gap-y-1 rounded-lg border border-neutral-400 px-3 py-4 dark:border-neutral-700"
+            >
+              <h2 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">
+                Color Palettes
+              </h2>
+              <p className="text-sm text-neutral-500 dark:text-neutral-500">
+                Carefully chosen sets of colors used to evoke specific emotions,
+                establish brand identity, or create a particular aesthetic in
+                visual designs.
+              </p>
+            </Link>
+          </li>
+          <li className="inline-grid">
+            <Link
+              href="/color-shades"
+              className="action grid content-baseline gap-y-1 rounded-lg border border-neutral-400 px-3 py-4 dark:border-neutral-700"
+            >
+              <h2 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">
+                Color Shades
+              </h2>
+              <p className="text-sm text-neutral-500 dark:text-neutral-500">
+                Encompass the spectrum of that color&apos;s lightness and
+                darkness, creating a gradient from its purest form to its
+                darkest possible version.
+              </p>
+            </Link>
+          </li>
+        </ul>
+      </section>
+      <Separator className="my-4" />
       <section
         aria-label="web colors"
         className="grid w-full max-w-3xl gap-y-3"
