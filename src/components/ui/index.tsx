995c54d9bfeee043879eadb866d743d26e83938b
r-sp <36327242+r-sp@users.noreply.github.com>
2025-01-06 20:55:24 +0700

    clean legacy codes before implement new structure

--- src/components/ui/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as Wrapper } from "./wrapper";
-export { default as Separator } from "./separator";
-export { default as Sticky } from "./sticky";
-export { default as Progress } from "./progress";
-export { default as Textarea } from "./textarea";
