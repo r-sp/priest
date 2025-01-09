74fd6790551f1c80ffb28484af43ec220d26315c
r-sp <36327242+r-sp@users.noreply.github.com>
2025-01-09 14:44:22 +0700

    add hex and color validation

--- src/utils/prefix.ts
+++ src/utils/prefix.ts
@@ -1,13 +1,22 @@
-import { parseCss, parseHex } from "~/lib/parse";
-
 export const isClient = typeof window !== "undefined";
 export const isServer = typeof window === "undefined";
 
-export const isValidHex = (hex: string): string => {
-  const color = parseCss(hex);
-  if (typeof color === "object") {
-    return parseHex(color);
-  } else {
-    return hex;
-  }
+export const isValidHex = (color: string): boolean => {
+  const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const letters = ["a", "b", "c", "d", "e", "f"];
+  const hex = ["#", ...numbers, ...letters];
+  const check = hex.find((letter) => color.startsWith(letter));
+  const valid = check ? true : false;
+  return valid;
+};
+
+export const isValidColor = (color: string): boolean => {
+  const space = ["r", "h", "l", "o"];
+  const check = space.find((letter) => color.startsWith(letter));
+  const valid = check ? true : false;
+  return valid;
+};
+
+export const isValidCss = (input: string): [boolean, boolean] => {
+  return [isValidHex(input), isValidColor(input)];
 };
