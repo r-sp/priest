4d99654d0f8a75950a6d630110bbec9da05f27df
r-sp <36327242+r-sp@users.noreply.github.com>
2025-01-19 23:20:34 +0700

    add color picker

--- src/components/ui/mode.tsx
+++ src/components/ui/mode.tsx
@@ -7,74 +7,79 @@ export default function ColorMode() {
   const [mode, setMode] = useMode();
 
   return (
-    <fieldset
-      role="radiogroup"
-      aria-label="color mode"
-      className="flex flex-wrap items-center gap-3 font-mono text-sm font-medium select-none"
+    <div
+      role="none"
+      className="no-scrollbar -mx-4 -my-2 grid overflow-x-auto px-4 py-2 whitespace-nowrap"
     >
-      <div className="inline-flex">
-        <Chip
-          prefix="rgb"
-          label="RGB"
-          group="mode"
-          isActive={mode === "rgb"}
-          onChange={() => setMode("rgb")}
-        />
-      </div>
-      <div className="inline-flex">
-        <Chip
-          prefix="hsl"
-          label="HSL"
-          group="mode"
-          isActive={mode === "hsl"}
-          onChange={() => setMode("hsl")}
-        />
-      </div>
-      <div className="inline-flex">
-        <Chip
-          prefix="hwb"
-          label="HWB"
-          group="mode"
-          isActive={mode === "hwb"}
-          onChange={() => setMode("hwb")}
-        />
-      </div>
-      <div className="inline-flex">
-        <Chip
-          prefix="lch"
-          label="LCH"
-          group="mode"
-          isActive={mode === "lch"}
-          onChange={() => setMode("lch")}
-        />
-      </div>
-      <div className="inline-flex">
-        <Chip
-          prefix="oklch"
-          label="OKLCH"
-          group="mode"
-          isActive={mode === "oklch"}
-          onChange={() => setMode("oklch")}
-        />
-      </div>
-      <div className="inline-flex">
-        <Chip
-          prefix="lab"
-          label="LAB"
-          group="mode"
-          isActive={mode === "lab"}
-          onChange={() => setMode("lab")}
-        />
-      </div>
-      <div className="inline-flex">
-        <Chip
-          prefix="oklab"
-          label="OKLAB"
-          group="mode"
-          isActive={mode === "oklab"}
-          onChange={() => setMode("oklab")}
-        />
-      </div>
-    </fieldset>
+      <fieldset
+        role="radiogroup"
+        aria-label="color mode"
+        className="flex flex-nowrap items-center gap-3 font-mono text-sm font-medium select-none"
+      >
+        <div className="inline-flex">
+          <Chip
+            prefix="rgb"
+            label="RGB"
+            group="mode"
+            isActive={mode === "rgb"}
+            onChange={() => setMode("rgb")}
+          />
+        </div>
+        <div className="inline-flex">
+          <Chip
+            prefix="hsl"
+            label="HSL"
+            group="mode"
+            isActive={mode === "hsl"}
+            onChange={() => setMode("hsl")}
+          />
+        </div>
+        <div className="inline-flex">
+          <Chip
+            prefix="hwb"
+            label="HWB"
+            group="mode"
+            isActive={mode === "hwb"}
+            onChange={() => setMode("hwb")}
+          />
+        </div>
+        <div className="inline-flex">
+          <Chip
+            prefix="lch"
+            label="LCH"
+            group="mode"
+            isActive={mode === "lch"}
+            onChange={() => setMode("lch")}
+          />
+        </div>
+        <div className="inline-flex">
+          <Chip
+            prefix="oklch"
+            label="OKLCH"
+            group="mode"
+            isActive={mode === "oklch"}
+            onChange={() => setMode("oklch")}
+          />
+        </div>
+        <div className="inline-flex">
+          <Chip
+            prefix="lab"
+            label="LAB"
+            group="mode"
+            isActive={mode === "lab"}
+            onChange={() => setMode("lab")}
+          />
+        </div>
+        <div className="inline-flex">
+          <Chip
+            prefix="oklab"
+            label="OKLAB"
+            group="mode"
+            isActive={mode === "oklab"}
+            onChange={() => setMode("oklab")}
+          />
+        </div>
+      </fieldset>
+    </div>
   );
 }
