f99351a7f6133c65af7205c93426f4845ec2ee5f
r-sp <36327242+r-sp@users.noreply.github.com>
2025-01-25 21:47:24 +0700

    add gamut range and predefine options

--- src/lib/gamut.ts
+++ src/lib/gamut.ts
@@ -1,4 +1,80 @@
-import type { AnyColorMode } from "./color";
+import type { ColorFormat, AnyColorMode } from "./color";
+
+type ColorGamut = {
+  [Key in ColorFormat]: {
+    get: (
+      color: Extract<AnyColorMode, { mode: Key }>,
+    ) => Extract<AnyColorMode, { mode: Key }>;
+    set: (
+      color: Extract<AnyColorMode, { mode: Key }>,
+    ) => Extract<AnyColorMode, { mode: Key }>;
+  };
+};
+
+const colorGamut: ColorGamut = {
+  rgb: {
+    get: (color) => ({
+      mode: "rgb",
+      r: color.r / 255,
+      g: color.g / 255,
+      b: color.b / 255,
+    }),
+    set: (color) => ({
+      mode: "rgb",
+      r: color.r * 255,
+      g: color.g * 255,
+      b: color.b * 255,
+    }),
+  },
+  hsl: {
+    get: (color) => ({
+      mode: "hsl",
+      h: color.h,
+      s: color.s / 100,
+      l: color.l / 100,
+    }),
+    set: (color) => ({
+      mode: "hsl",
+      h: color.h,
+      s: color.s * 100,
+      l: color.l * 100,
+    }),
+  },
+  hwb: {
+    get: (color) => ({
+      mode: "hwb",
+      h: color.h,
+      w: color.w / 100,
+      b: color.b / 100,
+    }),
+    set: (color) => ({
+      mode: "hwb",
+      h: color.h,
+      w: color.w * 100,
+      b: color.b * 100,
+    }),
+  },
+  lab: { get: (color) => color, set: (color) => color },
+  lch: { get: (color) => color, set: (color) => color },
+  oklab: { get: (color) => color, set: (color) => color },
+  oklch: { get: (color) => color, set: (color) => color },
+};
+
+const increaseGamut = <T extends ColorFormat>(
+  mode: T,
+): ((
+  color: Extract<AnyColorMode, { mode: T }>,
+) => Extract<AnyColorMode, { mode: T }>) => {
+  return colorGamut[mode].set;
+};
+
+const decreaseGamut = <T extends ColorFormat>(
+  mode: T,
+): ((
+  color: Extract<AnyColorMode, { mode: T }>,
+) => Extract<AnyColorMode, { mode: T }>) => {
+  return colorGamut[mode].get;
+};
 
 export const getGamut = (
   color: string | AnyColorMode,
@@ -7,124 +83,98 @@ export const getGamut = (
     return color;
   }
 
-  if (color.mode === "rgb") {
-    return {
-      mode: "rgb",
-      r: color.r / 255,
-      g: color.g / 255,
-      b: color.b / 255,
-    };
-  } else if (color.mode === "hsl") {
-    return {
-      mode: "hsl",
-      h: color.h,
-      s: color.s / 100,
-      l: color.l / 100,
-    };
-  } else if (color.mode === "hwb") {
-    return {
-      mode: "hwb",
-      h: color.h,
-      w: color.w / 100,
-      b: color.b / 100,
-    };
-  } else {
-    return color;
-  }
+  const compose = decreaseGamut(color.mode);
+  return compose(color);
 };
 
 export const setGamut = (color: AnyColorMode): AnyColorMode => {
-  if (color.mode === "rgb") {
-    return {
-      mode: "rgb",
-      r: color.r * 255,
-      g: color.g * 255,
-      b: color.b * 255,
-    };
-  } else if (color.mode === "hsl") {
-    return {
-      mode: "hsl",
-      h: color.h,
-      s: color.s * 100,
-      l: color.l * 100,
-    };
-  } else if (color.mode === "hwb") {
-    return {
-      mode: "hwb",
-      h: color.h,
-      w: color.w * 100,
-      b: color.b * 100,
-    };
-  } else {
-    return color;
+  const compose = increaseGamut(color.mode);
+  return compose(color);
+};
+
+let currentGamut: string | null = null;
+
+const getGamutRange = (
+  label: string,
+  value: number,
+  min: number,
+  max: number,
+): string | null => {
+  if (value < min) {
+    return `${label}-under-${min}`;
+  } else if (value > max) {
+    return `${label}-above-${max}`;
+  }
+  return null;
+};
+
+const setGamutRange = (
+  label: string,
+  value: number,
+  min: number,
+  max: number,
+) => {
+  const invalid = getGamutRange(label, value, min, max);
+  if (invalid) {
+    if (!currentGamut) {
+      currentGamut = invalid;
+    } else {
+      currentGamut += `-and-${invalid}`;
+    }
   }
 };
 
+type FormatGamut = {
+  [Key in ColorFormat]: (color: Extract<AnyColorMode, { mode: Key }>) => void;
+};
+
+export const formatGamut: FormatGamut = {
+  rgb: (color) => {
+    setGamutRange("red", color.r, 0, 255);
+    setGamutRange("green", color.g, 0, 255);
+    setGamutRange("blue", color.b, 0, 255);
+  },
+  hsl: (color) => {
+    setGamutRange("hue", color.h!, 0, 360);
+    setGamutRange("saturation", color.s, 0, 100);
+    setGamutRange("lightness", color.l, 0, 100);
+  },
+  hwb: (color) => {
+    setGamutRange("hue", color.h!, 0, 360);
+    setGamutRange("whiteness", color.w, 0, 100);
+    setGamutRange("blackness", color.b, 0, 100);
+  },
+  lab: (color) => {
+    setGamutRange("lightness", color.l, 0, 100);
+    setGamutRange("green-red", color.a, -100, 100);
+    setGamutRange("blue-yellow", color.b, -100, 100);
+  },
+  lch: (color) => {
+    setGamutRange("lightness", color.l, 0, 100);
+    setGamutRange("chroma", color.c, 0, 150);
+    setGamutRange("hue", color.h!, 0, 360);
+  },
+  oklab: (color) => {
+    setGamutRange("lightness", color.l, 0, 1);
+    setGamutRange("green-red", color.a, -0.4, 0.4);
+    setGamutRange("blue-yellow", color.b, -0.4, 0.4);
+  },
+  oklch: (color) => {
+    setGamutRange("lightness", color.l, 0, 1);
+    setGamutRange("chroma", color.c, 0, 0.4);
+    setGamutRange("hue", color.h!, 0, 360);
+  },
+};
+
+const gamutMode = <T extends ColorFormat>(
+  mode: T,
+): ((color: Extract<AnyColorMode, { mode: T }>) => void) => {
+  return formatGamut[mode];
+};
+
 export const checkGamut = (color: AnyColorMode): string | null => {
-  let reports: string | null = null;
-
-  const revalidate = (
-    label: string,
-    value: number,
-    min: number,
-    max: number,
-  ): string | null => {
-    if (value < min) {
-      return `${label}-under-${min}`;
-    } else if (value > max) {
-      return `${label}-above-${max}`;
-    }
-    return null;
-  };
-
-  const check = (label: string, value: number, min: number, max: number) => {
-    const invalid = revalidate(label, value, min, max);
-    if (invalid) {
-      if (!reports) {
-        reports = invalid;
-      } else {
-        reports += `-and-${invalid}`;
-      }
-    }
-  };
-
-  switch (color.mode) {
-    case "rgb":
-      check("red", color.r, 0, 255);
-      check("green", color.g, 0, 255);
-      check("blue", color.b, 0, 255);
-      break;
-    case "hsl":
-      check("hue", color.h!, 0, 360);
-      check("saturation", color.s, 0, 100);
-      check("lightness", color.l, 0, 100);
-      break;
-    case "hwb":
-      check("hue", color.h!, 0, 360);
-      check("whiteness", color.w, 0, 100);
-      check("blackness", color.b, 0, 100);
-      break;
-    case "lab":
-      check("lightness", color.l, 0, 100);
-      check("green-red", color.a, -100, 100);
-      check("blue-yellow", color.b, -100, 100);
-      break;
-    case "lch":
-      check("lightness", color.l, 0, 100);
-      check("chroma", color.c, 0, 150);
-      check("hue", color.h!, 0, 360);
-      break;
-    case "oklab":
-      check("lightness", color.l, 0, 1);
-      check("green-red", color.a, -0.4, 0.4);
-      check("blue-yellow", color.b, -0.4, 0.4);
-      break;
-    case "oklch":
-      check("lightness", color.l, 0, 1);
-      check("chroma", color.c, 0, 0.4);
-      check("hue", color.h!, 0, 360);
-      break;
-  }
-
-  return reports;
+  const compose = gamutMode(color.mode);
+  currentGamut = "";
+  compose(color);
+  return currentGamut;
 };
