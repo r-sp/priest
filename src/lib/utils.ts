6a56179a2b22cb01108f85ea062e5d455c3b5512
r-sp <36327242+r-sp@users.noreply.github.com>
2025-01-25 23:39:20 +0700

    debug mode (#106)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,24 +1,6 @@
 export const isClient = typeof window !== "undefined";
 export const isServer = typeof window === "undefined";
 
-export const isValidHex = (color: string): boolean => {
-  const hex = ["#"];
-  const check = hex.find((letter) => color.startsWith(letter));
-  const valid = check ? true : false;
-  return valid;
-};
-
-export const isValidColor = (color: string): boolean => {
-  const space = ["rgb", "hsl", "hwb", "lab", "lch", "oklab", "oklch"];
-  const check = space.find((letter) => color.includes(letter));
-  const valid = check ? true : false;
-  return valid;
-};
-
-export const isValidCss = (input: string): [boolean, boolean] => {
-  return [isValidHex(input), isValidColor(input)];
-};
-
 export const round = (value: number, digits: number = 0): number => {
   if (digits === 0) {
     return Math.round(value);
