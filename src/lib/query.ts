b8130f6618b35230dc8e6d18ba7dfed062fd65d5
r-sp <36327242+r-sp@users.noreply.github.com>
2025-01-25 22:23:41 +0700

    add color query and path with switch statements

--- src/lib/query.ts
+++ src/lib/query.ts
@@ -3,158 +3,116 @@ import type { ColorQuery, AnyColorMode } from "./color";
 export const getColorQuery = (query: ColorQuery): AnyColorMode | undefined => {
   const { mode, r, g, b, h, s, l, w, a, c } = query;
 
-  if (mode === "rgb" && r !== undefined && g !== undefined && b !== undefined) {
-    return { mode, r, g, b };
-  } else if (
-    mode === "hsl" &&
-    h !== undefined &&
-    s !== undefined &&
-    l !== undefined
-  ) {
-    return { mode, h, s, l };
-  } else if (
-    mode === "hwb" &&
-    h !== undefined &&
-    w !== undefined &&
-    b !== undefined
-  ) {
-    return { mode, h, w, b };
-  } else if (
-    mode === "lab" &&
-    l !== undefined &&
-    a !== undefined &&
-    b !== undefined
-  ) {
-    return { mode, l, a, b };
-  } else if (
-    mode === "lch" &&
-    l !== undefined &&
-    c !== undefined &&
-    h !== undefined
-  ) {
-    return { mode, l, c, h };
-  } else if (
-    mode === "oklab" &&
-    l !== undefined &&
-    a !== undefined &&
-    b !== undefined
-  ) {
-    return { mode, l, a, b };
-  } else if (
-    mode === "oklch" &&
-    l !== undefined &&
-    c !== undefined &&
-    h !== undefined
-  ) {
-    return { mode, l, c, h };
-  } else {
-    return undefined;
-  }
-};
-
-export const getColorPath = (path: string, query: ColorQuery): string => {
-  const { mode, r, g, b, h, s, l, w, a, c } = query;
-  const searchParams = new URLSearchParams([]);
-
-  if (mode === "rgb" && r !== undefined && g !== undefined && b !== undefined) {
-    searchParams.set("mode", mode);
-    searchParams.set("r", `${r}`);
-    searchParams.set("g", `${g}`);
-    searchParams.set("b", `${b}`);
-  }
-  if (mode === "hsl" && h !== undefined && s !== undefined && l !== undefined) {
-    searchParams.set("mode", mode);
-    searchParams.set("h", `${h}`);
-    searchParams.set("s", `${s}`);
-    searchParams.set("l", `${l}`);
-  }
-  if (mode === "hwb" && h !== undefined && w !== undefined && b !== undefined) {
-    searchParams.set("mode", mode);
-    searchParams.set("h", `${h}`);
-    searchParams.set("w", `${w}`);
-    searchParams.set("b", `${b}`);
-  }
-  if (mode === "lab" && l !== undefined && a !== undefined && b !== undefined) {
-    searchParams.set("mode", mode);
-    searchParams.set("l", `${l}`);
-    searchParams.set("a", `${a}`);
-    searchParams.set("b", `${b}`);
-  }
-  if (mode === "lch" && l !== undefined && c !== undefined && h !== undefined) {
-    searchParams.set("mode", mode);
-    searchParams.set("l", `${l}`);
-    searchParams.set("c", `${c}`);
-    searchParams.set("h", `${h}`);
-  }
-  if (
-    mode === "oklab" &&
-    l !== undefined &&
-    a !== undefined &&
-    b !== undefined
-  ) {
-    searchParams.set("mode", mode);
-    searchParams.set("l", `${l}`);
-    searchParams.set("a", `${a}`);
-    searchParams.set("b", `${b}`);
-  }
-  if (
-    mode === "oklch" &&
-    l !== undefined &&
-    c !== undefined &&
-    h !== undefined
-  ) {
-    searchParams.set("mode", mode);
-    searchParams.set("l", `${l}`);
-    searchParams.set("c", `${c}`);
-    searchParams.set("h", `${h}`);
-  }
-
-  return `${path}?${searchParams.toString()}`;
-};
-
-export const switchColorPath = (path: string, color: AnyColorMode): string => {
-  const searchParams = new URLSearchParams([]);
-  searchParams.set("mode", color.mode);
-
-  switch (color.mode) {
+  switch (mode) {
     case "rgb": {
-      const { r, g, b } = color;
-      searchParams.set("r", `${r}`);
-      searchParams.set("g", `${g}`);
-      searchParams.set("b", `${b}`);
+      if (r !== undefined && g !== undefined && b !== undefined) {
+        return { mode, r, g, b };
+      }
       break;
     }
     case "hsl": {
-      const { h, s, l } = color;
-      searchParams.set("h", `${h}`);
-      searchParams.set("s", `${s}`);
-      searchParams.set("l", `${l}`);
+      if (h !== undefined && s !== undefined && l !== undefined) {
+        return { mode, h, s, l };
+      }
       break;
     }
     case "hwb": {
-      const { h, w, b } = color;
-      searchParams.set("h", `${h}`);
-      searchParams.set("w", `${w}`);
-      searchParams.set("b", `${b}`);
+      if (h !== undefined && w !== undefined && b !== undefined) {
+        return { mode, h, w, b };
+      }
       break;
     }
     case "lab":
     case "oklab": {
-      const { l, a, b } = color;
-      searchParams.set("l", `${l}`);
-      searchParams.set("a", `${a}`);
-      searchParams.set("b", `${b}`);
+      if (l !== undefined && a !== undefined && b !== undefined) {
+        return { mode, l, a, b };
+      }
       break;
     }
     case "lch":
     case "oklch": {
-      const { l, c, h } = color;
-      searchParams.set("l", `${l}`);
-      searchParams.set("c", `${c}`);
-      searchParams.set("h", `${h}`);
+      if (l !== undefined && c !== undefined && h !== undefined) {
+        return { mode, l, c, h };
+      }
+      break;
+    }
+    default: {
+      return undefined;
+    }
+  }
+
+  return undefined;
+};
+
+export const getColorPath = (path: string, query: ColorQuery): string => {
+  const { mode, r, g, b, h, s, l, w, a, c } = query;
+  let queryString = "";
+
+  switch (mode) {
+    case "rgb": {
+      if (r !== undefined && g !== undefined && b !== undefined) {
+        queryString = `mode=${mode}&r=${r}&g=${g}&b=${b}`;
+      }
+      break;
+    }
+    case "hsl": {
+      if (h !== undefined && s !== undefined && l !== undefined) {
+        queryString = `mode=${mode}&h=${h}&s=${s}&l=${l}`;
+      }
+      break;
+    }
+    case "hwb": {
+      if (h !== undefined && w !== undefined && b !== undefined) {
+        queryString = `mode=${mode}&h=${h}&w=${w}&b=${b}`;
+      }
+      break;
+    }
+    case "lab":
+    case "oklab": {
+      if (l !== undefined && a !== undefined && b !== undefined) {
+        queryString = `mode=${mode}&l=${l}&a=${a}&b=${b}`;
+      }
+      break;
+    }
+    case "lch":
+    case "oklch": {
+      if (l !== undefined && c !== undefined && h !== undefined) {
+        queryString = `mode=${mode}&l=${l}&c=${c}&h=${h}`;
+      }
       break;
     }
   }
 
-  return `${path}?${searchParams.toString()}`;
+  return queryString ? `${path}?${queryString}` : path;
+};
+
+export const switchColorPath = (path: string, color: AnyColorMode): string => {
+  let queryString = `mode=${color.mode}`;
+
+  switch (color.mode) {
+    case "rgb": {
+      queryString += `&r=${color.r}&g=${color.g}&b=${color.b}`;
+      break;
+    }
+    case "hsl": {
+      queryString += `&h=${color.h}&s=${color.s}&l=${color.l}`;
+      break;
+    }
+    case "hwb": {
+      queryString += `&h=${color.h}&w=${color.w}&b=${color.b}`;
+      break;
+    }
+    case "lab":
+    case "oklab": {
+      queryString += `&l=${color.l}&a=${color.a}&b=${color.b}`;
+      break;
+    }
+    case "lch":
+    case "oklch": {
+      queryString += `&l=${color.l}&c=${color.c}&h=${color.h}`;
+      break;
+    }
+  }
+
+  return `${path}?${queryString}`;
 };
