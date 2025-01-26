63f3778fac4eaf2a0b9bb41057300276dae67e1c
r-sp <36327242+r-sp@users.noreply.github.com>
2025-01-26 23:04:49 +0700

    add debug and theme as common components

---
+++ src/components/common/index.ts
@@ -0,0 +1,2 @@
+export { ThemeScript, ThemeSwitcher } from "./theme";
+export { default as DebugMode } from "./debug";
--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Slider } from "./common/slider";
-export { default as Chip } from "./common/chip";
-
-export { default as ColorPicker } from "./ui/picker";
