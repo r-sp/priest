9de0789fa8be280412c4c9fbd2e8f80c4cb510d7
r-sp <36327242+r-sp@users.noreply.github.com>
2025-02-24 21:40:59 +0700

    fix not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,33 @@
-import { type Metadata } from "next";
+import type { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "404: Error",
-  description: "Page Not Found",
+  title: "404 - Page Not Found",
+  description:
+    "The requested URL was not found on this server. Double-check the URL or try searching for what you're looking for.",
 };
 
 export default function NotFoundPage() {
   return (
     <div className="px-4">
-      <div className="max-w-8xl mx-auto">
-        <h1>
-          <code>404: Error</code>
+      <article className="mx-auto grid w-full max-w-3xl py-16">
+        <h1 className="text-xl font-medium text-gray-900 dark:text-gray-100">
+          We&apos;re sorry, but the page you were looking for could not be
+          found.
         </h1>
-        <p>Page Not Found</p>
-      </div>
+        <p className="mt-4 text-lg text-gray-600 dark:text-gray-400">
+          It may have been removed, had its name changed, or is temporarily
+          unavailable.
+        </p>
+        <div className="mt-6 flex flex-wrap select-none">
+          <Link
+            href="/"
+            className="action inline-flex h-9 items-center justify-center rounded-md px-4 text-sm ring"
+          >
+            Go to Homepage
+          </Link>
+        </div>
+      </article>
     </div>
   );
 }
