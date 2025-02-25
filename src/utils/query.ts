dd277d63eea3b4760a98ef72e61a5285d67f5ffc
r-sp <36327242+r-sp@users.noreply.github.com>
2025-02-25 21:21:33 +0700

    fix metadata color queries

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -50,68 +50,72 @@ const getColorPath = (path: string, query: ColorQuery): string => {
   const { mode, r, g, b, h, s, l, w, a, c } = query;
   let queryString = "";
 
+  if (mode) {
+    queryString += `mode=${mode}&`;
+  }
+
   switch (mode) {
     case "rgb": {
       if (r !== undefined && g !== undefined && b !== undefined) {
-        queryString = `mode=${mode}&r=${r}&g=${g}&b=${b}`;
+        queryString += `r=${r}&g=${g}&b=${b}`;
       }
       break;
     }
     case "hsl": {
       if (h !== undefined && s !== undefined && l !== undefined) {
-        queryString = `mode=${mode}&h=${h}&s=${s}&l=${l}`;
+        queryString += `h=${h}&s=${s}&l=${l}`;
       }
       break;
     }
     case "hwb": {
       if (h !== undefined && w !== undefined && b !== undefined) {
-        queryString = `mode=${mode}&h=${h}&w=${w}&b=${b}`;
+        queryString += `h=${h}&w=${w}&b=${b}`;
       }
       break;
     }
     case "lab":
     case "oklab": {
       if (l !== undefined && a !== undefined && b !== undefined) {
-        queryString = `mode=${mode}&l=${l}&a=${a}&b=${b}`;
+        queryString += `l=${l}&a=${a}&b=${b}`;
       }
       break;
     }
     case "lch":
     case "oklch": {
       if (l !== undefined && c !== undefined && h !== undefined) {
-        queryString = `mode=${mode}&l=${l}&c=${c}&h=${h}`;
+        queryString += `l=${l}&c=${c}&h=${h}`;
       }
       break;
     }
   }
 
-  return queryString ? `${path}?${queryString}` : path;
+  return queryString === "" ? path : `${path}?${queryString}`;
 };
 
 const switchColorPath = (path: string, color: AnyColorMode): string => {
-  let queryString = `mode=${color.mode}`;
+  let queryString = `mode=${color.mode}&`;
 
   switch (color.mode) {
     case "rgb": {
-      queryString += `&r=${color.r}&g=${color.g}&b=${color.b}`;
+      queryString += `r=${color.r}&g=${color.g}&b=${color.b}`;
       break;
     }
     case "hsl": {
-      queryString += `&h=${color.h}&s=${color.s}&l=${color.l}`;
+      queryString += `h=${color.h}&s=${color.s}&l=${color.l}`;
       break;
     }
     case "hwb": {
-      queryString += `&h=${color.h}&w=${color.w}&b=${color.b}`;
+      queryString += `h=${color.h}&w=${color.w}&b=${color.b}`;
       break;
     }
     case "lab":
     case "oklab": {
-      queryString += `&l=${color.l}&a=${color.a}&b=${color.b}`;
+      queryString += `l=${color.l}&a=${color.a}&b=${color.b}`;
       break;
     }
     case "lch":
     case "oklch": {
-      queryString += `&l=${color.l}&c=${color.c}&h=${color.h}`;
+      queryString += `l=${color.l}&c=${color.c}&h=${color.h}`;
       break;
     }
   }
@@ -123,37 +127,43 @@ const createMetadata = (query: ColorQuery & { error?: string }): Metadata => {
   const includes =
     query.error?.includes("under") || query.error?.includes("above");
 
-  const createMetadata = (valid: string, invalid: string) => {
+  const createColor = (
+    invalid: string,
+    fn: (input: AnyColorMode) => string,
+  ) => {
+    const repaint: ColorQuery =
+      query.error || query.mode ? query : { mode: "rgb", r: 0, g: 0, b: 0 };
+
+    return query.error
+      ? includes
+        ? fn(getColorQuery(repaint)!)
+        : invalid
+      : fn(getColorQuery(repaint)!);
+  };
+
+  const createText = (valid: string, invalid: string) => {
     return query.error ? (includes ? valid : invalid) : valid;
   };
 
-  const repaint: ColorQuery =
-    query.error || query.mode ? query : { mode: "rgb", r: 0, g: 0, b: 0 };
+  const createPath = (q: ColorQuery) => {
+    const param = getColorPath("/color", q);
+    return query.error
+      ? includes
+        ? `${param}&error=${query.error}`
+        : `/color?error=${query.error}`
+      : param;
+  };
 
-  const color = query.error
-    ? includes
-      ? formatCss(getColorQuery(repaint)!)
-      : "Error"
-    : formatCss(getColorQuery(repaint)!);
+  const color = createColor("Error", formatCss);
 
-  const hex = query.error
-    ? includes
-      ? createHex(getColorQuery(repaint)!)
-      : "#696969"
-    : createHex(getColorQuery(repaint)!);
+  const hex = createColor("#696969", createHex);
 
-  const name = createMetadata(`Color: ${color}`, "Error");
-  const text = createMetadata(
+  const name = createText(`Color: ${color}`, "Error");
+  const text = createText(
     `Explore color conversions from ${color} to various color models: RGB, HSL, HWB, LAB, LCH, OKLAB, OKLCH.`,
     "Something",
   );
-
-  const colorParams = getColorPath("/color", query);
-  const path = query.error
-    ? includes
-      ? `${colorParams}&error=${query.error}`
-      : `/color?error=${query.error}`
-    : colorParams;
+  const path = createPath(query);
 
   return {
     title: name,
