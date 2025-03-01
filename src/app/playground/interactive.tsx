4f1d672795779b4f33279e9abd64366a37f890cf
r-sp <36327242+r-sp@users.noreply.github.com>
2025-03-01 23:39:12 +0700

    interactive axis to color (#141)

--- src/app/playground/interactive.tsx
+++ src/app/playground/interactive.tsx
@@ -140,20 +140,52 @@ function Interactive() {
     };
   }, [handleResize]);
 
-  // test
+  // converting x-y to hsv to hsl
+  const fixup = (x: number, y: number) => {
+    x /= 100;
+    y /= 100;
+    x = Math.max(0, Math.min(1, x));
+    y = 1 - Math.max(0, Math.min(1, y));
+    let l = y * (1 - x / 2);
+    let s;
+    if (l === 0 || l === 1) {
+      s = 0;
+    } else {
+      s = (y - l) / Math.min(l, 1 - l);
+    }
+    s *= 100;
+    l *= 100;
+    return [s, l];
+  };
+
+  const [s, l] = fixup(position.x, position.y);
+
+  const hue = 216;
+  const saturation = round(s, 2);
+  const lightness = round(l, 2);
+
   const btnX = round(position.x, 2);
   const btnY = round(position.y, 2);
+
+  const css = `hsl(${hue}deg ${saturation}% ${lightness}%)`;
+  const ring = `hsl(${hue}deg ${saturation}% ${lightness > 40 ? 20 : 80}%)`;
 
   return (
     <motion.div
       ref={boxRef}
       layout="preserve-aspect"
-      className="relative z-0 flex aspect-square overflow-hidden select-none"
+      className="bg-ref relative z-0 flex aspect-square overflow-hidden select-none"
+      style={{
+        ["--bg" as string]: `hsl(${hue}deg 100% 50%)`,
+        backgroundImage:
+          "linear-gradient(0deg,#000,transparent),linear-gradient(90deg,#fff,hsla(0,0%,100%,0))",
+        boxShadow: "inset 0 0 0 1px rgba(0,0,0,.05)",
+      }}
     >
       <div
         className="absolute top-0 right-0 bottom-0 left-0 z-1 cursor-crosshair"
-        onClick={handleTapRadius}
         tabIndex={-1}
+        onClick={handleTapRadius}
       >
         <motion.div
           animate={animationControls}
@@ -163,14 +195,21 @@ function Interactive() {
           dragElastic={false}
           dragMomentum={true}
           onDrag={(e) => handleDrag(e)}
-          className="absolute z-2 size-8 cursor-all-scroll rounded-2xl bg-green-600/70"
+          className="bg-ref absolute z-2 size-8 cursor-all-scroll rounded-2xl ring-2"
+          style={{
+            ["--bg" as string]: css,
+            ["--tw-ring-color" as string]: ring,
+          }}
           tabIndex={0}
         />
       </div>
-      <div className="pointer-events-none absolute top-0 right-0 bottom-0 left-0 z-0 flex items-center justify-center bg-gray-400">
-        <div className="inline-flex size-8 rounded-2xl bg-red-600/70" />
-      </div>
-      <div className="pointer-events-none absolute bottom-0 z-2 flex flex-col gap-y-2 px-4 pb-4 font-mono text-sm break-words text-gray-700">
+      <div className="pointer-events-none absolute bottom-0 z-2 flex flex-col gap-y-2 px-4 pb-4 font-mono text-sm break-words text-gray-200 mix-blend-difference">
+        <div className="inline-flex flex-col">
+          <h2>Color</h2>
+          <p>{`H: ${hue}deg`}</p>
+          <p>{`S: ${saturation}%`}</p>
+          <p>{`L: ${lightness}%`}</p>
+        </div>
         <div className="inline-flex flex-col">
           <h2>Motion</h2>
           <p>{`X: ${btnX}%`}</p>
