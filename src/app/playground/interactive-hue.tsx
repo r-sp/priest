44ea45ae4a75fe6bf7376c4d7351e558a235128e
r-sp <36327242+r-sp@users.noreply.github.com>
2025-03-05 23:52:34 +0700

    fix broken interactive color picker (#145)

--- src/app/playground/interactive-hue.tsx
+++ src/app/playground/interactive-hue.tsx
@@ -1,227 +1,187 @@
 "use client";
 
-import type { MouseEvent } from "react";
-import type { InteractiveProps } from "./interactive-color";
-import { useRef, useCallback, useEffect, memo } from "react";
+import type { StoreStates, StoreActions } from "./store";
+import { useReducer, useRef, useCallback, useEffect, memo } from "react";
 import { useAnimationControls, motion } from "motion/react";
+import { storeReducer } from "./store";
 import { round } from "~/utils";
 
-function Interactive({ state, dispatch }: InteractiveProps) {
-  const trackRef = useRef<HTMLDivElement>(null);
-  const animationControls = useAnimationControls();
+interface Props {
+  color: [number, number, number];
+}
 
-  const getTrackRect = useCallback(() => {
-    const hasTrackRef = trackRef.current;
-    if (!hasTrackRef) {
-      return {
-        x: 0,
-        y: 0,
-        width: 0,
-        height: 0,
-        top: 0,
-        left: 0,
-      } as DOMRect;
-    }
-    return hasTrackRef.getBoundingClientRect();
-  }, [trackRef]);
+function Interactive({ color }: Props) {
+  const [hue] = color;
 
-  const getTrackRadius = useCallback(
-    (event: globalThis.PointerEvent | MouseEvent) => {
-      const area = event.target as HTMLDivElement;
-      const ignoredArea = Number(area.getAttribute("tabindex") ?? -1) === 0;
-
-      const trackRect = getTrackRect();
-
-      const clickX = event.clientX - trackRect.left;
-      const percentX = (clickX / trackRect.width) * 100;
-
-      return [percentX, ignoredArea] as [number, boolean];
+  const [state, dispatch] = useReducer<StoreStates, [StoreActions]>(
+    storeReducer,
+    {
+      box: { left: 0, top: 0, width: 0, height: 24 },
+      btn: { left: 0, top: 0, width: 24, height: 24 },
     },
-    [getTrackRect],
   );
 
-  const getTrackAxis = useCallback(
-    (percentX: number) => {
-      const trackWidth = state.track.width;
-      const offsetX = (percentX / 100 - 0.5) * trackWidth;
-
-      const pointX = trackWidth / 2 + offsetX;
-      const pointZ = (percentX / 100) * 360;
-
-      return [pointX, pointZ] as [number, number];
-    },
-    [state],
-  );
-
-  const handleDrag = useCallback(
-    (
-      event:
-        | globalThis.MouseEvent
-        | globalThis.TouchEvent
-        | globalThis.PointerEvent,
-    ) => {
-      const trackRect = getTrackRect();
-
-      const trackWidth = state.track.width;
-      const thumbLeft = state.thumb.left;
-
-      const hasThumb = event.target as HTMLDivElement;
-      const thumbRect = hasThumb.getBoundingClientRect();
-
-      const offsetLeft = thumbRect.left - trackRect.left;
-
-      dispatch({
-        type: "hue",
-        h: (thumbLeft / trackWidth) * 360,
-        s: state.color.s,
-        v: state.color.v,
-        left: offsetLeft,
-        top: 0,
-        width: thumbRect.width,
-        height: thumbRect.height,
-      });
-    },
-    [getTrackRect, state, dispatch],
-  );
-
-  const handlePosition = useCallback(
-    (percentX: number) => {
-      const [pointX, pointZ] = getTrackAxis(percentX);
-      animationControls.start({ x: pointX, y: 0 });
-
-      const hasHue = state.color.h !== pointZ;
-      if (hasHue) {
-        dispatch({
-          type: "color",
-          h: pointZ,
-          s: state.color.s,
-          v: state.color.v,
-        });
-      }
-    },
-    [getTrackAxis, animationControls, state, dispatch],
-  );
-
-  const handlePanRadius = useCallback(
-    (event: globalThis.PointerEvent) => {
-      const [percentX, ignoredArea] = getTrackRadius(event);
-      if (ignoredArea) return;
-
-      const [pointX, pointZ] = getTrackAxis(percentX);
-      animationControls.set({ x: pointX, y: 0 });
-
-      const hasHue = state.color.h !== pointZ;
-      if (hasHue) {
-        dispatch({
-          type: "color",
-          h: pointZ,
-          s: state.color.s,
-          v: state.color.v,
-        });
-      }
-    },
-    [getTrackRadius, getTrackAxis, animationControls, state, dispatch],
-  );
-
-  const handleTapRadius = useCallback(
-    (event: MouseEvent) => {
-      const [percentX, ignoredArea] = getTrackRadius(event);
-      if (ignoredArea) return;
-
-      handlePosition(percentX);
-    },
-    [getTrackRadius, handlePosition],
-  );
-
-  const getTrackPosition = useCallback(() => {
-    const trackRect = getTrackRect();
-
-    const thumbWidth = state.track.width;
-    const thumbHeight = state.track.height;
-
-    const offsetWidth = trackRect.width - thumbWidth;
-    const offsetHeight = trackRect.height - thumbHeight;
-
-    const offset = {
-      l: trackRect.left,
-      w: offsetWidth,
-      h: offsetHeight,
-      t: trackRect.top,
-    };
-
-    const x = (state.color.h / 360) * offsetWidth;
-
-    const hasHue = x !== state.color.h;
-
-    return [offset, x, hasHue] as [typeof offset, number, boolean];
-  }, [getTrackRect, state]);
+  const constraint = useRef<HTMLDivElement | null>(null);
+  const animation = useAnimationControls();
 
   const handleResize = useCallback(() => {
-    const [offset, axis, hasHue] = getTrackPosition();
+    const hasConstraint = constraint.current;
+    if (!hasConstraint) return;
 
-    if (hasHue) {
-      animationControls.start({
-        x: axis,
-        scale: 1,
-        opacity: 1,
-      });
+    const hasBtn = hasConstraint.querySelector("button");
+    if (!hasBtn) return;
 
-      dispatch({
-        type: "track",
-        left: offset.l,
-        top: offset.t,
-        width: offset.w,
-        height: offset.h,
-      });
-    }
-  }, [getTrackPosition, animationControls, dispatch]);
+    const boxRect = hasConstraint.getBoundingClientRect();
+    const btnRect = hasBtn.getBoundingClientRect();
+
+    const boxWidth = boxRect.width - state.btn.width;
+
+    const pointX = (hue / 360) * boxWidth;
+
+    animation.set({
+      x: pointX,
+      y: 0,
+    });
+    dispatch({
+      type: "viewport",
+      box: {
+        left: boxRect.left,
+        top: boxRect.top,
+        width: boxWidth,
+        height: boxRect.height,
+      },
+      btn: {
+        left: pointX,
+        top: 0,
+        width: btnRect.width,
+        height: btnRect.height,
+      },
+    });
+  }, [state, dispatch, hue, animation]);
 
   useEffect(() => {
-    const controls = new AbortController();
-    if (!controls.signal.aborted) {
-      handleResize();
-    }
     window.addEventListener("resize", handleResize);
     return () => {
-      controls.abort();
       window.removeEventListener("resize", handleResize);
     };
   }, [handleResize]);
 
-  const hue = round(state.color.h, 2);
+  const percentX = (state.btn.left / state.box.width) * 360;
+  const pointX = (state.btn.left / state.box.width) * 100;
+
+  const h = round(percentX, 2);
+
+  const css = `hsl(${h}deg 100% 50%)`;
 
   return (
-    <div className="relative z-0 flex h-6 items-center overflow-hidden rounded-xl select-none">
-      <motion.div
-        ref={trackRef}
-        layout="preserve-aspect"
-        className="absolute top-0 right-0 bottom-0 left-0 z-1 cursor-crosshair touch-none"
-        tabIndex={-1}
-        onPan={handlePanRadius}
-        onClick={handleTapRadius}
+    <div>
+      <div
+        ref={constraint}
+        className="bg-ref relative z-0 flex h-6 w-full items-center overflow-hidden select-none"
       >
         <motion.div
-          animate={animationControls}
-          initial={{ scale: 0.5, opacity: 0.1 }}
-          drag="x"
-          dragConstraints={trackRef}
-          dragTransition={{ bounceStiffness: 10, bounceDamping: 2 }}
-          dragDirectionLock={true}
-          dragElastic={false}
-          dragMomentum={true}
-          className="bg-ref size-6 cursor-all-scroll rounded-xl ring-2 shadow-gray-500 ring-gray-950/50 ring-inset dark:ring-gray-50/70"
-          style={{ ["--bg" as string]: `hsl(${hue}deg 100% 50%)` }}
-          tabIndex={0}
-          onDrag={(e) => handleDrag(e)}
+          layout="preserve-aspect"
+          layoutId="color-hue"
+          viewport={{ once: true }}
+          className="absolute top-0 right-0 bottom-0 left-0 z-1 inline-flex cursor-crosshair touch-none outline-0"
+          tabIndex={-1}
+          onViewportEnter={handleResize}
+        >
+          <motion.button
+            animate={animation}
+            drag
+            dragConstraints={constraint}
+            dragTransition={{ bounceStiffness: 10, bounceDamping: 2 }}
+            dragElastic={false}
+            dragMomentum={true}
+            initial={{ opacity: 0 }}
+            viewport={{ once: true }}
+            whileInView={{ opacity: 1 }}
+            aria-label="hue"
+            className="bg-ref absolute z-2 size-6 cursor-all-scroll rounded-2xl ring-2 outline-0 ring-inset"
+            style={{ ["--bg" as string]: css }}
+            tabIndex={0}
+            onDrag={(event) => {
+              event.preventDefault();
+              const hasBtn = event.target as HTMLDivElement;
+              const btnRect = hasBtn.getBoundingClientRect();
+
+              const pointX = btnRect.left - state.box.left;
+
+              dispatch({ type: "drag", left: pointX, top: 0 });
+            }}
+          />
+        </motion.div>
+        <div
+          className="bg-gradient-ref pointer-events-none absolute right-0 left-0 z-0 h-4 rounded-xl"
+          style={{
+            ["--bg" as string]:
+              "linear-gradient(90deg, hsl(0 100% 50%) 0%, hsl(60 100% 50%) 17%, hsl(120 100% 50%) 33%, hsl(180 100% 50%) 50%, hsl(240 100% 50%) 67%, hsl(300 100% 50%) 83%, hsl(0 100% 50%) 100%)",
+          }}
         />
-      </motion.div>
-      <div
-        className="bg-gradient-ref pointer-events-none absolute right-0 left-0 z-0 h-4 rounded-xl"
-        style={{
-          ["--bg" as string]:
-            "linear-gradient(90deg, hsl(0 100% 50%) 0%, hsl(60 100% 50%) 17%, hsl(120 100% 50%) 33%, hsl(180 100% 50%) 50%, hsl(240 100% 50%) 67%, hsl(300 100% 50%) 83%, hsl(0 100% 50%) 100%)",
-        }}
-      />
+      </div>
+      <Preview point={{ x: pointX, h: h }} box={state.box} btn={state.btn} />
+    </div>
+  );
+}
+
+interface Dataset {
+  point: { x: number; h: number };
+  box: StoreStates["box"];
+  btn: StoreStates["btn"];
+}
+
+function Preview({ point, box, btn }: Dataset) {
+  const pointX = round(point.x, 2);
+  const pointH = round(point.h, 2);
+
+  const btnLeft = round(btn.left, 2);
+  const btnTop = round(btn.top, 2);
+  const btnWidth = round(btn.width, 2);
+  const btnHeight = round(btn.height, 2);
+
+  const boxLeft = round(box.left, 2);
+  const boxTop = round(box.top, 2);
+  const boxWidth = round(box.width, 2);
+  const boxHeight = round(box.height, 2);
+
+  const hasOffset = (
+    value: number,
+    min: number,
+    max: number,
+  ): string | undefined => {
+    const warning = "text-red-700 dark:text-red-400";
+    const above = value > max;
+    const under = value < min;
+    return above || under ? warning : undefined;
+  };
+
+  return (
+    <div className="grid gap-4 pt-4 font-mono text-gray-600 md:grid-cols-2 dark:text-gray-400">
+      <div className="flex flex-col">
+        <h2 className="font-semibold text-gray-800 dark:text-gray-200">axis</h2>
+        <p className={hasOffset(pointX, 0, 100)}>{`X: ${pointX}%`}</p>
+      </div>
+      <div className="flex flex-col">
+        <h2 className="font-semibold text-gray-800 dark:text-gray-200">
+          color
+        </h2>
+        <p className={hasOffset(pointH, 0, 360)}>{`H: ${pointH}%`}</p>
+      </div>
+      <div className="flex flex-col">
+        <h2 className="font-semibold text-gray-800 dark:text-gray-200">box</h2>
+        <p>{`L: ${boxLeft}`}</p>
+        <p>{`T: ${boxTop}`}</p>
+        <p>{`W: ${boxWidth}px`}</p>
+        <p>{`H: ${boxHeight}px`}</p>
+      </div>
+      <div className="flex flex-col">
+        <h2 className="font-semibold text-gray-800 dark:text-gray-200">btn</h2>
+        <p className={hasOffset(btnLeft, 0, boxWidth)}>{`L: ${btnLeft}`}</p>
+        <p className={hasOffset(btnTop, 0, boxHeight)}>{`T: ${btnTop}`}</p>
+        <p>{`W: ${btnWidth}px`}</p>
+        <p>{`H: ${btnHeight}px`}</p>
+      </div>
     </div>
   );
 }
--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -13,9 +13,10 @@ export default function Page() {
     <div className="px-4 py-8">
       <article className="mx-auto grid w-full max-w-5xl gap-y-6">
         <h1 className="sr-only">Playground</h1>
-        <div className="grid gap-8 md:grid-cols-2">
-          <ColorPicker />
-        </div>
+        <ColorPicker />
+        <div className="grid aspect-square"></div>
+        <div className="grid aspect-square"></div>
+        <div className="grid aspect-square"></div>
       </article>
     </div>
   );
